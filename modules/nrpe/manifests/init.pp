f2b65d96dc6e97ed3a00cb1314347a8038a6b5f1
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-01-13 14:54:07 -0500

    Make nrpe module compatible with mcollective module by updating nrpe path

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -19,28 +19,33 @@ define nrpe_command ($command, $parameters='', $cplugdir='auto', $ensure='presen
   case $ensure {
     'absent': {
       file {
-        "/etc/nagios/nrpe.d/${name}.cfg":
+        "${nrpe::nrpe_dir}/${name}.cfg":
           ensure => absent
       }
     }
     default:    {
       file {
-        "/etc/nagios/nrpe.d/${name}.cfg":
+        "${nrpe::nrpe_dir}/${name}.cfg":
           owner   => root,
           group   => root,
           mode    => 644,
           content => template('nrpe/nrpe-config.erb'),
-          require => File['/etc/nagios/nrpe.d'],
+          require => File["$nrpe::nrpe_dir"],
       }
     }
   }
 }
 
 class nrpe {
+
+  #this allows system to use both nrpe and mcollective
+  $nrpe_dir = $::operatingsystem ? {
+      /(?i-mx:centos|fedora|redhat|oel)/ => '/etc/nrpe.d',
+      default                            => '/etc/nagios/nrpe.d',
+  }
+
   file {
-    '/etc/nagios':
-      ensure => directory;
-    '/etc/nagios/nrpe.d':
+    ['/etc/nagios',"${nrpe_dir}"]:
       ensure => directory;
   }
 
@@ -89,33 +94,21 @@ class nrpe {
   nrpe_command {
     'check_disks':
       command    => 'check_disk',
-      parameters => '--warning=10% --critical=5% --path=/ --local --units GB'
-  }
-
-  nrpe_command {
+      parameters => '--warning=10% --critical=5% --path=/ --local --units GB';
     'check_ntp':
       command    => 'check_ntp_time',
-      parameters => '-H 147.11.100.50 -w 0.5 -c 1'
-  }
-
-  #make sure the nx log file has been updated recently. Checks if nx is hung
-  nrpe_command {
+      parameters => '-H 147.11.100.50 -w 0.5 -c 1';
+    #make sure the nx log file has been updated recently. Checks if nx is hung
     'check_nx':
       command    => 'check_file_age',
-      parameters => '-w 21600  -c 43200 -f /home/buildadmin/log/nx.log'
-  }
-
-  #make sure puppet has successully run on system recently
-  nrpe_command {
+      parameters => '-w 21600  -c 43200 -f /home/buildadmin/log/nx.log';
+    #make sure puppet has successully run on system recently
     'check_puppet':
       command    => 'check_file_age',
-      parameters => '-w 7200  -c 14400 -f /var/lib/puppet/state/state.yaml'
-  }
-
-  #check whether there is an nx process running on the machine
-  nrpe_command {
+      parameters => '-w 7200  -c 14400 -f /var/lib/puppet/state/state.yaml';
+    #check whether there is an nx process running on the machine
     'check_nx_proc':
       command    => 'check_procs',
-      parameters => '-c 1:4 -C nx'
+      parameters => '-c 1:4 -C nx';
   }
 }
