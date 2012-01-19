6e7ccaa8ded808a2ccdab25aae1901a5bdded952
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-01-19 15:31:14 -0500

    Fix nodes file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,7 @@
 node default {
 }
 
-node master_common {
+node 'ala-lpd-puppet.wrs.com' inherits master_common {
   class { redhat: }
   class { java: distribution => 'java-1.6.0-openjdk' }
   class { activemq: broker_name => 'ala-broker' }
@@ -9,11 +9,18 @@ node master_common {
   class { wr::master: }
 }
 
-node 'ala-lpd-puppet.wrs.com' inherits master_common {
-}
-
 node 'pek-lpd-puppet.wrs.com' inherits master_common {
+  class { redhat: }
+  class { java: distribution => 'java-1.6.0-openjdk' }
+  class { activemq: broker_name => 'pek-broker' }
+  class { wr::mcollective: }
+  class { wr::master: }
 }
 
 node 'yow-lpd-puppet.ottawa.wrs.com' inherits master_common {
+  class { redhat: }
+  class { java: distribution => 'java-1.6.0-openjdk' }
+  class { activemq: broker_name => 'yow-broker' }
+  class { wr::mcollective: }
+  class { wr::master: }
 }
