4dc1ac4d09176d8e660de0d166c1b421ba95c83e
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-01-19 15:43:30 -0500

    ActiveMQ not needed on yow-lpd-puppet

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -20,7 +20,6 @@ node 'pek-lpd-puppet.wrs.com' {
 node 'yow-lpd-puppet.ottawa.wrs.com' {
   class { redhat: }
   class { java: distribution => 'java-1.6.0-openjdk' }
-  class { activemq: broker_name => 'yow-broker' }
   class { wr::mcollective: }
   class { wr::master: }
 }
