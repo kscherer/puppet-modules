4e7da53ba9e1e8d5af88f8cf42898903c7ee6ac4
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-02-02 16:08:04 -0500

    Fix nis module to use file_line

--- modules/nis/manifests/init.pp
+++ modules/nis/manifests/init.pp
@@ -10,36 +10,36 @@ class nis {
   include network
 
   #Note this is for redhat machines only
-  line {
+  file_line {
     'nisdomain':
-      file   => '/etc/sysconfig/network',
-      line   => 'NISDOMAIN=swamp',
       ensure => present,
+      path   => '/etc/sysconfig/network',
+      line   => 'NISDOMAIN=swamp',
       notify => Service['network'];
   }
 
   file {
     '/etc/yp.conf':
       content => 'domain swamp server 128.224.144.20',
-      owner   => root, group => root, mode => 644,
+      owner   => root, group => root, mode => '0644',
       require => Package['ypbind'];
     '/etc/nsswitch.conf':
       source  => 'puppet:///modules/nis/nsswitch.conf',
-      owner   => root, group => root, mode => 644,
+      owner   => root, group => root, mode => '0644',
       require => Package['ypbind'];
     '/etc/auto.master':
       source  => 'puppet:///modules/nis/auto.master',
-      owner   => root, group => root, mode => 644,
+      owner   => root, group => root, mode => '0644',
       require => Package['autofs'];
     ['/net','/folk']:
-      notify  => Service['autofs'],
-      ensure  => directory;
+      ensure  => directory,
+      notify  => Service['autofs'];
   }
 
   service {
     'ypbind':
-      enable     => true,
       ensure     => running,
+      enable     => true,
       hasrestart => true,
       hasstatus  => true,
       require    => [ File['/etc/yp.conf'], File['/etc/nsswitch.conf'],
@@ -48,12 +48,11 @@ class nis {
 
   service {
     'autofs':
-      enable     => true,
       ensure     => running,
+      enable     => true,
       hasrestart => true,
       hasstatus  => true,
       require    => [ File['/etc/auto.master'], Package['autofs'],
                       File['/folk']];
   }
-
 }
