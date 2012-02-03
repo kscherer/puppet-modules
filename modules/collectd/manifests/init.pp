ad429aed474d82a9e7f31bdd311aceef2d9f635e
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-02-03 15:14:36 -0500

    Reverted to previous simpler collectd module

--- modules/collectd/manifests/init.pp
+++ modules/collectd/manifests/init.pp
@@ -1,45 +1,28 @@
-# collectd/manifests/init.pp
-# (C) Copyright: 2008, David Schmitt <david@dasz.at>
-
-# Module: collectd
-#
-# To start managing the collectd, include the collectd class in your node. Use
-# the collectd::conf define to set basic parameters of your installation.
-# collectd::plugin is the foundation of many plugin-specific defines which help
-# you configuring their respective plugin.
-#
 # Class: collectd
-# Manages the installation and running of a collectd as well as the
-# /etc/collectd/collectd.conf file.
+#
+# This class installs and configures collectd
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#   - The collectd::params class
+#
+# Sample Usage:
+#
 class collectd {
+  include collectd::params
 
-  libdir { ['collectd', 'collectd/plugins', 'collectd/thresholds' ]: }
-
-  package {
-    'collectd':
-      ensure => installed;
+  package { 'collectd':
+    ensure => present,
   }
 
-  service {
-    'collectd':
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      pattern    => collectd,
-      require    => Package['collectd'];
+  service { 'collectd':
+    ensure => running,
+    enable     => true,
+    hasrestart => true,
+    require => Package['collectd'];
   }
 
-  file {
-    '/etc/collectd/collectd.conf':
-      ensure  => present,
-      mode    => '0644', owner => root, group => 0,
-      require => Package['collectd'],
-      notify  => Service['collectd'];
-  }
-
-  collectd::conf {
-    'Include':
-      value => [ "${::module_dir_path}/collectd/plugins/*.conf",
-                  "${::module_dir_path}/collectd/thresholds/*.conf"];
-  }
 }
