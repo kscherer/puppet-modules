188890480cc89027acf0e512b3de068e2a3b2835
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-03-13 11:41:51 -0400

    Support ensure absent for apt::source

--- modules/apt/manifests/source.pp
+++ modules/apt/manifests/source.pp
@@ -10,49 +10,52 @@ define apt::source(
   $key = false,
   $key_server = 'keyserver.ubuntu.com',
   $pin = false,
+  $ensure = 'file',
   $key_content = false
 ) {
 
   include apt::params
 
-
   file { "${name}.list":
-    name => "${apt::params::root}/sources.list.d/${name}.list",
-    ensure => file,
-    owner => root,
-    group => root,
-    mode => 644,
-    content => template("apt/source.list.erb"),
-  }
-
-  if $pin != false {
-    apt::pin { "${release}": priority => "${pin}" }
+    ensure  => $ensure,
+    name    => "${apt::params::root}/sources.list.d/${name}.list",
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('apt/source.list.erb'),
   }
 
   exec { "${name} apt update":
-    command => "${apt::params::provider} update",
-    subscribe => File["${name}.list"],
+    command     => "${apt::params::provider} update",
+    subscribe   => File["${name}.list"],
     refreshonly => true,
   }
 
-  if $required_packages != false {
-    exec { "${apt::params::provider} -y install ${required_packages}":
-      subscribe => File["${name}.list"],
-      refreshonly => true,
-    }
-  }
+  if $ensure != 'absent' {
 
-  if $key != false {
-    if $key_content {
-      exec { "Add key: ${key} from content":
-        command => "/bin/echo '${key_content}' | /usr/bin/apt-key add -",
-        unless => "/usr/bin/apt-key list | /bin/grep '${key}'",
-        before => File["${name}.list"],
-      } 
-    } else {
-      exec { "/usr/bin/apt-key adv --keyserver ${key_server} --recv-keys ${key}":
-        unless => "/usr/bin/apt-key list | /bin/grep ${key}",
-        before => File["${name}.list"],
+    if $pin != false {
+      apt::pin { $release: priority => $pin }
+    }
+
+    if $required_packages != false {
+      exec { "${apt::params::provider} -y install ${required_packages}":
+        subscribe   => File["${name}.list"],
+        refreshonly => true,
+      }
+    }
+
+    if $key != false {
+      if $key_content {
+        exec { "Add key: ${key} from content":
+          command => "/bin/echo '${key_content}' | /usr/bin/apt-key add -",
+          unless  => "/usr/bin/apt-key list | /bin/grep '${key}'",
+          before  => File["${name}.list"],
+        }
+      } else {
+        exec { "/usr/bin/apt-key adv --keyserver ${key_server} --recv-keys ${key}":
+          unless => "/usr/bin/apt-key list | /bin/grep ${key}",
+          before => File["${name}.list"],
+        }
       }
     }
   }
