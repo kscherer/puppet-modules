85538f7b3e67b98826dc5fb1f4b9e62e5b25e4ea
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-04-18 10:22:59 -0400

    Update repos to use CDC mirror if host is in CDC

--- modules/redhat/manifests/repos.pp
+++ modules/redhat/manifests/repos.pp
@@ -32,10 +32,14 @@ class redhat::repos {
       'redhat-el5', 'rhel-debuginfo', 'centos_dvd' ]:
   }
 
-  $yow_mirror = 'http://yow-mirror.wrs.com/mirror'
-  $yow_mrepo_mirror = "${yow_mirror}/mrepo/repos"
+  $mirror = $::hostname ? {
+    /^pek.*$/ => 'http://pek-mirror.wrs.com/mirror',
+    default   => 'http://yow-mirror.wrs.com/mirror',
+  }
+
+  $mrepo_mirror = "${mirror}/mrepo/repos"
   $redhat_dvd_repo = "redhat-${::operatingsystemrelease}-${::architecture}-repo"
-  $fedora_repo_base = "${yow_mrepo_mirror}/fedora${::operatingsystemrelease}-${::architecture}"
+  $fedora_repo_base = "${mrepo_mirror}/fedora${::operatingsystemrelease}-${::architecture}"
 
   #this exists solely to stop yum complaining about missing name
   define redhat::named_yumrepo( $baseurl ){
@@ -50,43 +54,41 @@ class redhat::repos {
   #relevant platforms
   redhat::named_yumrepo {
     "epel-el4-$::architecture":
-      baseurl => "${yow_mrepo_mirror}/rhel5c-${::architecture}/RPMS.epel";
+      baseurl => "${mrepo_mirror}/rhel5c-${::architecture}/RPMS.epel";
     "epel-el5-$::architecture":
-      baseurl => "${yow_mrepo_mirror}/rhel5c-${::architecture}/RPMS.epel";
+      baseurl => "${mrepo_mirror}/rhel5c-${::architecture}/RPMS.epel";
     "epel-el6-${::architecture}":
-      baseurl => "${yow_mrepo_mirror}/rhel6ws-${::architecture}/RPMS.epel";
+      baseurl => "${mrepo_mirror}/rhel6ws-${::architecture}/RPMS.epel";
     'redhat-dvd':
-      baseurl => "${yow_mirror}/repos/${redhat_dvd_repo}";
+      baseurl => "${mirror}/repos/${redhat_dvd_repo}";
     'centos-dvd':
       #This is a link to the latest CentOS DVD release for 6
       #and 5.5 for CentOS 5 as later versions of Redhat are
       #officially not supported by wrlinux
-      baseurl => "$yow_mirror/repos/centos-${::lsbmajdistrelease}-${::architecture}";
+      baseurl => "$mirror/repos/centos-${::lsbmajdistrelease}-${::architecture}";
     'fedora-updates':
       baseurl => "${fedora_repo_base}/RPMS.updates";
     'fedora-everything':
       baseurl => "${fedora_repo_base}/RPMS.everything";
-    'mcollective':
-      baseurl => "${yow_mirror}/mcollective";
     'rhel6-optional':
-      baseurl => "${yow_mrepo_mirror}/rhel6ws-${::architecture}/RPMS.optional";
+      baseurl => "${mrepo_mirror}/rhel6ws-${::architecture}/RPMS.optional";
     'rhel6-updates':
-      baseurl => "${yow_mrepo_mirror}/rhel6ws-${::architecture}/RPMS.updates";
+      baseurl => "${mrepo_mirror}/rhel6ws-${::architecture}/RPMS.updates";
     'centos5-updates':
-      baseurl => "${yow_mrepo_mirror}/centos5-${::architecture}/RPMS.updates";
+      baseurl => "${mrepo_mirror}/centos5-${::architecture}/RPMS.updates";
     'centos6-updates':
-      baseurl => "${yow_mrepo_mirror}/centos6-${::architecture}/RPMS.updates";
+      baseurl => "${mrepo_mirror}/centos6-${::architecture}/RPMS.updates";
     'puppetlabs-rh4':
-      baseurl => "${yow_mrepo_mirror}/puppetlabs-rh5-${::architecture}/RPMS.both";
+      baseurl => "${mrepo_mirror}/puppetlabs-rh5-${::architecture}/RPMS.both";
     'puppetlabs-rh5':
-      baseurl => "${yow_mrepo_mirror}/puppetlabs-rh5-${::architecture}/RPMS.both";
+      baseurl => "${mrepo_mirror}/puppetlabs-rh5-${::architecture}/RPMS.both";
     'puppetlabs-fedora':
       baseurl =>
-        "${yow_mrepo_mirror}/puppetlabs-f${::operatingsystemrelease}-${::architecture}/RPMS.both";
+        "${mrepo_mirror}/puppetlabs-f${::operatingsystemrelease}-${::architecture}/RPMS.both";
     'puppetlabs-rh6':
-      baseurl => "${yow_mrepo_mirror}/puppetlabs-rh6-${::architecture}/RPMS.both";
+      baseurl => "${mrepo_mirror}/puppetlabs-rh6-${::architecture}/RPMS.both";
     'passenger-rh6':
-      baseurl => "${yow_mrepo_mirror}/passenger-rh6-${::architecture}/RPMS.all";
+      baseurl => "${mrepo_mirror}/passenger-rh6-${::architecture}/RPMS.all";
   }
 
   case $::operatingsystemrelease {
@@ -97,7 +99,6 @@ class redhat::repos {
   }
 
   #setup repos depending on which flavour of redhat
-  realize( Yumrepo['mcollective'] )
   case $::operatingsystem {
     CentOS: {
       realize( Yumrepo['centos-dvd'] )
