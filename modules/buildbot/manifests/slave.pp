ef866bb293483b657bbb762568afa26959434006
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-04-24 08:28:51 -0400

    More package to support yocto

--- modules/buildbot/manifests/slave.pp
+++ modules/buildbot/manifests/slave.pp
@@ -100,8 +100,10 @@ class buildbot::slave(
   }
 
   #buildbot needs some packages not part of wrlinux required files
+  #glibc_std in yocto requires OpenGL
   package {
-    ['texi2html','chrpath','diffstat','subversion']:
+    [ 'texi2html', 'chrpath','diffstat','subversion','libgl1-mesa-dev', 'libglu1-mesa-dev',
+      'libsdl1.2-dev']:
       ensure => installed;
   }
 }
