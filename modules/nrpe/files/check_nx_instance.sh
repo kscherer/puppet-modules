95aedf8bf96876671a32d6c52a157ca22430aab9
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-04-26 14:44:55 -0400

    Change nx instance to report STFO deployment as normal for puppet

--- modules/nrpe/files/check_nx_instance.sh
+++ modules/nrpe/files/check_nx_instance.sh
@@ -5,31 +5,35 @@ num_warning=0
 num_critical=0
 longoutput=
 
-for instance in {1..4}; do
-
-    nx=/etc/init.d/nx_instance.$instance
-    if [ -a $nx ]; then
-        output=$($nx status)
-        if [ "$?" -eq "0" ]; then
-            num_ok=$(($num_ok+1))
-        elif [ "$?" -eq "1" ]; then
-            num_warning=$((num_warning+1))
-            longoutput="$longoutput\n$output"
-        elif [ "$?" -eq "150" ]; then
-            num_warning=$((num_warning+1))
-            longoutput="$longoutput\n$output"
-        elif [ "$?" -eq "151" ]; then
-            num_warning=$((num_warning+1))
-            longoutput="$longoutput\n$output"
-        elif [ "$?" -eq "3" ]; then
-            num_critical=$(($num_critical+1))
-            longoutput="$longoutput\n$output"
-        elif [ "$?" -eq "4" ]; then
-            num_critical=$(($num_critical+1))
-            longoutput="$longoutput\n$output"
+if [ -f /var/tmp/STFU ]; then
+    num_warning=1
+    longoutput="SFTU Deployed"
+else
+    for instance in {1..4}; do
+        nx=/etc/init.d/nx_instance.$instance
+        if [ -a $nx ]; then
+            output=$($nx status)
+            if [ "$?" -eq "0" ]; then
+                num_ok=$(($num_ok+1))
+            elif [ "$?" -eq "1" ]; then
+                num_warning=$((num_warning+1))
+                longoutput="$longoutput\n$output"
+            elif [ "$?" -eq "150" ]; then
+                num_warning=$((num_warning+1))
+                longoutput="$longoutput\n$output"
+            elif [ "$?" -eq "151" ]; then
+                num_warning=$((num_warning+1))
+                longoutput="$longoutput\n$output"
+            elif [ "$?" -eq "3" ]; then
+                num_critical=$(($num_critical+1))
+                longoutput="$longoutput\n$output"
+            elif [ "$?" -eq "4" ]; then
+                num_critical=$(($num_critical+1))
+                longoutput="$longoutput\n$output"
+            fi
         fi
-    fi
-done
+    done
+fi
 
 total=$(($num_ok+$num_warning+$num_critical))
 
