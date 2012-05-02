70a84bd9dde61679a2595e889bd6eac9bef0587d
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-05-02 14:54:54 -0400

    Add packages needed for yocto on redhat

---
+++ modules/yocto/manifests/redhat.pp
@@ -0,0 +1,9 @@
+class yocto::redhat {
+  #buildbot needs some packages not part of wrlinux required files
+  #glibc_std in yocto requires OpenGL
+  package {
+    [ 'texi2html', 'chrpath','diffstat','subversion']:
+        ensure => installed;
+  }
+
+}
