756149fbfce02aea4d02270db0f9537e49cb6f95
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-05-03 11:12:21 -0400

    OpenGL packages for Fedora

--- modules/yocto/manifests/redhat.pp
+++ modules/yocto/manifests/redhat.pp
@@ -2,7 +2,8 @@ class yocto::redhat {
   #buildbot needs some packages not part of wrlinux required files
   #glibc_std in yocto requires OpenGL
   package {
-    [ 'texi2html', 'chrpath','diffstat','subversion']:
+    [ 'texi2html', 'chrpath','diffstat','subversion','mesa-libGL', 'mesa-libGLU',
+      'SDL-devel']:
         ensure => installed;
   }
 
