e87f963d2d0085c30c0ce2a2afa40360453a0352
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-05-22 22:12:44 -0400

    Add help2man package to suppress warnings during build

--- modules/yocto/manifests/debian.pp
+++ modules/yocto/manifests/debian.pp
@@ -3,7 +3,7 @@ class yocto::debian {
   #glibc_std in yocto requires OpenGL
   package {
     [ 'texi2html', 'chrpath','diffstat','subversion','libgl1-mesa-dev', 'libglu1-mesa-dev',
-      'libsdl1.2-dev', 'texinfo', 'gawk', 'gcc']:
+      'libsdl1.2-dev', 'texinfo', 'gawk', 'gcc', 'help2man']:
         ensure => installed;
   }
 
