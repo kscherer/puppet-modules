0b47c8cd797b86835ee7d7a9ef8d6ca0d95315db
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-05-28 10:34:45 -0400

    Rework Yocto package list on Suse

--- modules/yocto/manifests/suse.pp
+++ modules/yocto/manifests/suse.pp
@@ -1,9 +1,11 @@
 class yocto::suse {
-  #buildbot needs some packages not part of wrlinux required files
-  #glibc_std in yocto requires OpenGL
-  package {
-    [ 'chrpath','diffstat','subversion','Mesa', 'Mesa-devel',
-      'libSDL-devel', 'texinfo', 'gawk', 'gcc', 'gcc-c++', 'help2man']:
-        ensure => installed;
+  if $::operatingsystem == 'OpenSuSE' and $::operatingsystemrelease == '12.1' {
+    #buildbot needs some packages not part of wrlinux required files
+    #glibc_std in yocto requires OpenGL
+    package {
+      [ 'chrpath','diffstat','subversion','Mesa', 'Mesa-devel',
+        'libSDL-devel', 'texinfo', 'gawk', 'gcc', 'gcc-c++', 'help2man']:
+          ensure => installed;
+    }
   }
 }
