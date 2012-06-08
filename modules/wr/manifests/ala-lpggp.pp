6ddf58ae8ca7c27249d0ebd062cca3b421104c74
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-06-08 14:28:47 -0400

    Make links for Ting

--- modules/wr/manifests/ala-lpggp.pp
+++ modules/wr/manifests/ala-lpggp.pp
@@ -3,4 +3,18 @@ class wr::ala-lpggp inherits wr::ala-common {
   class { 'yocto': }
   Class['redhat'] -> Class['yocto']
 
+  if $::hostname == 'ala-lpggp2.wrs.com' {
+    file {
+      '/lpg-build':
+        ensure => link,
+        target => '/ala-lpggp22';
+    }
+  }
+  if $::hostname == 'ala-lpggp3.wrs.com' {
+    file {
+      '/lpg-build':
+        ensure => link,
+        target => '/ala-lpggp32';
+    }
+  }
 }
