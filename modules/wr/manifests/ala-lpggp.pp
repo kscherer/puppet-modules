914867285cd1d04ce7a9dd1e7019aed8fcce2fe4
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-06-18 14:24:41 -0400

    Make sure latest git from epel is available

--- modules/wr/manifests/ala-lpggp.pp
+++ modules/wr/manifests/ala-lpggp.pp
@@ -17,4 +17,22 @@ class wr::ala-lpggp inherits wr::ala-common {
         target => '/ala-lpggp32';
     }
   }
+
+  #make sure latest git is available from epel
+  package {
+    'git':
+      ensure => 'latest';
+  }
+
+  define wr::ala-lpggp::local_file() {
+    file {
+      "/usr/local/bin/$name":
+        ensure => 'absent';
+    }
+  }
+
+  wr::ala-lpggp::local_file {
+    [ '/git','gitk','git-cvsserver','git-shell','git-upload-pack',
+      'git-upload-archive','git-recieve-pack']:
+  }
 }
