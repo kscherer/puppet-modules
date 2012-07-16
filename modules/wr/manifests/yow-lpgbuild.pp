616eb05ae121ed60dceabee7ec1ce1eda95ed863
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-07-16 15:52:22 -0400

    Move ssh keys to nx base class

--- modules/wr/manifests/yow-lpgbuild.pp
+++ modules/wr/manifests/yow-lpgbuild.pp
@@ -11,23 +11,6 @@ class wr::yow-lpgbuild inherits wr::yow-common {
       password => '$6$p6ikdyj/GHN7Uno3$VDlbq91Mp5osT0yLxVTbtDhhidFYTK7r/2xM5426g6bbesNzfhaXditRBSieRwsgpNJIbYEQhA7SZcXdf.VcZ0';
   }
 
-  ssh_authorized_key {
-    'kscherer_windriver_nxadm':
-      ensure => 'present',
-      user   => 'nxadm',
-      key    => extlookup('kscherer@yow-kscherer-l1'),
-      type   => 'ssh-dss';
-    'kscherer_home_nxadm':
-      ensure => 'present',
-      user   => 'nxadm',
-      key    => extlookup('kscherer@helix'),
-      type   => 'ssh-rsa';
-    'jwessel_nxadm':
-      ensure => 'present',
-      user   => 'nxadm',
-      key    => extlookup('jwessel@splat'),
-      type   => 'ssh-rsa';
-  }
   motd::register{
     'yow-lpgbuild':
       content => 'This machine is reserved for WR Linux coverage builds.';
