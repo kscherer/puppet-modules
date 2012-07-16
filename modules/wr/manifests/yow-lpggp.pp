140560cca4f04cc40ad24c78de03f14853f68feb
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-07-16 14:15:03 -0400

    Attempt to fix formatting of motd

--- modules/wr/manifests/yow-lpggp.pp
+++ modules/wr/manifests/yow-lpggp.pp
@@ -23,11 +23,11 @@ class wr::yow-lpggp inherits wr::yow-common {
 
   motd::register{
     'yow-lpggp':
-      content => 'This machine is for Linux Products developers manual
-        compiles.  It is not to be used for automated testing, automated
-        builds or other uses. Please limit compiles to --enable-jobs=5.
-        Use /yow-lpggp11,2 as local storage.  It is not backed up, make
-        sure you have a secure copy of your data.  Clean up after
+      content => 'This machine is for Linux Products developers manual \
+        compiles.  It is not to be used for automated testing, automated \
+        builds or other uses. Please limit compiles to --enable-jobs=5. \
+        Use /yow-lpggp11,2 as local storage.  It is not backed up, make \
+        sure you have a secure copy of your data.  Clean up after \
         yourself, this F/S will be cleaned up periodically.';
   }
 }
