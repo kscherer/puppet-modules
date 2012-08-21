a2de3aea4d79be21f1f3f570c29e6ac1b76daed6
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-08-21 15:53:58 -0400

    Move yow-blade1 builds to local disks

--- modules/nx/manifests/yow-blades.pp
+++ modules/nx/manifests/yow-blades.pp
@@ -25,9 +25,39 @@ class nx::yow-blades {
   }
 
   case $::hostname {
-    /^yow-blade[1-8]$/: { include nx::netapp_iscsi_setup }
+    yow-blade1: { include nx::yow_local_build }
+    /^yow-blade[2-8]$/: { include nx::netapp_iscsi_setup }
     yow-blade9: { include nx::md3000_iscsi_setup }
     /^yow-blade1[0-6]$/: { include nx::md3000_iscsi_setup }
     default: { fail("Do not know how to configure nx for $::hostname")}
+  }
+}
+
+class nx::yow_local_build {
+
+  $local_builddir = '/test'
+
+  file {
+    $local_builddir:
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0755';
+    "${local_builddir}/nxadm":
+      ensure  => directory,
+      mode    => '0755';
+    "${local_builddir}/nxadm/nx":
+      ensure  => directory,
+      require => File["$local_builddir/nxadm"],
+      mode    => '0755';
+    '/home/nxadm/nx':
+      ensure  => link,
+      target  => "${local_builddir}/nxadm/nx";
+    "${local_builddir}/nxadm/nx/${::hostname}.1":
+      ensure  => directory,
+      mode    => '0755';
+    "${local_builddir}/nxadm/nx/${::hostname}.2":
+      ensure  => directory,
+      mode    => '0755';
   }
 }
