be86533e30ee18a53e55adbcbba7143f98f967fe
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-09-20 16:16:45 -0400

    Update nameserver for Ottawa

--- modules/wr/manifests/yow-common.pp
+++ modules/wr/manifests/yow-common.pp
@@ -20,6 +20,6 @@ class wr::yow-common inherits wr::mcollective {
     '/etc/resolv.conf':
       ensure  => present,
       mode    => '0644',
-      content => "domain wrs.com\nsearch wrs.com windriver.com corp.ad.wrs.com\nnameserver 128.224.144.28 \nnameserver 147.11.57.128\n";
+      content => "domain wrs.com\nsearch wrs.com windriver.com corp.ad.wrs.com\nnameserver 128.224.144.130 \nnameserver 147.11.57.128\n";
   }
 }
