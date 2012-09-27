f1ec9e8eec1e9b638f53b779045f810980f12978
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-09-27 10:27:38 -0400

    With changes in yaml output, use cron to generate facter.yaml to stop unnecessary change reports in Dashboard

--- modules/wr/manifests/mcollective.pp
+++ modules/wr/manifests/mcollective.pp
@@ -32,23 +32,13 @@ class wr::mcollective (
       }
   }
 
-  #If this runs early then some data will be out of date for one run
-  file { '/etc/mcollective/facter.yaml':
-    ensure   => file,
-    backup   => false,
-    replace  => true,
-    #grab all puppet vars from current scope, filter out invalid
-    #or frequently changing values, make a yaml hash
-    content  => inline_template("<%= scope.to_hash.reject {
-      |k,v| !k.is_a?(String) || !v.is_a?(String) ||
-      k.to_s =~ /(uptime|timestamp|free|path|rubysitedir|pubkey|ssh|module_name|caller_module_name)/
-      }.to_yaml() %>")
-  }
-
   cron {
     'restart_mcollective':
       command => '/sbin/service mcollective restart &> /dev/null',
       minute  => '0',
       hour    => '0';
+    'generate_facts':
+      command => '/usr/bin/facter --puppet --yaml > /etc/mcollective/facter.yaml 2> /dev/null',
+      minute  => '0';
   }
 }
