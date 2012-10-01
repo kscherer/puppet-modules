7d3a5b081eeaee4e434eed4e214378e931541081
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-10-01 11:56:53 -0400

    Removing VolGroup from nrpe check removes too many disks

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -113,7 +113,7 @@ class nrpe {
   nrpe::command {
     'check_disks':
       command    => 'check_disk',
-      parameters => '--warning=10% --critical=5% --all --ignore-eregi-path=\'(shm|boot|prebuilt_cache|VolGroup|folk)\' --units GB';
+      parameters => '--warning=10% --critical=5% --all --ignore-eregi-path=\'(shm|boot|prebuilt_cache|folk)\' --units GB';
     'check_ntp':
       command    => 'check_ntp_time',
       parameters => "-H $first_ntp_server -w 1.0 -c 2.0";
