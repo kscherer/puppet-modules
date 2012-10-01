128249bca7c01efec90ee55abe90868f4bb57fe5
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-10-01 10:54:00 -0400

    Enable nagios exported resources on ala machines

--- modules/wr/manifests/ala-common.pp
+++ modules/wr/manifests/ala-common.pp
@@ -13,11 +13,12 @@ class wr::ala-common inherits wr::mcollective {
   -> class { 'nrpe': }
   -> class { 'nis': }
 
+  class { 'nagios::target': }
+
   file {
     '/etc/resolv.conf':
       ensure  => present,
       mode    => '0644',
       content => "domain wrs.com\nsearch wrs.com windriver.com corp.ad.wrs.com\nnameserver 147.11.57.128\nnameserver 147.11.57.133\n";
   }
-
 }
