8002a2f57ccd81c0b97bdffbb772fbd2d7102abd
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-10-19 09:46:18 -0400

    Moving rest of yow-blades to local disk

--- modules/nx/manifests/yow-blades.pp
+++ modules/nx/manifests/yow-blades.pp
@@ -2,15 +2,8 @@
 class nx::yow-blades {
 
   #create nx instances
+  include nx::local_build
   nx::setup {
     [ '1', '2' ]:
-  }
-
-  case $::hostname {
-    yow-blade1: { include nx::local_build }
-    /^yow-blade[2-8]$/: { include nx::local_build }
-    yow-blade9: { include nx::md3000_iscsi_setup }
-    /^yow-blade1[0-6]$/: { include nx::md3000_iscsi_setup }
-    default: { fail("Do not know how to configure nx for $::hostname")}
   }
 }
