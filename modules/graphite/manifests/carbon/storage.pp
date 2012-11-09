c150ff92e23e989a2ddca857260b590bcc841ab4
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-11-09 10:53:57 -0500

    Add graphite to ala-lpd-puppet

--- modules/graphite/manifests/carbon/storage.pp
+++ modules/graphite/manifests/carbon/storage.pp
@@ -1,9 +1,10 @@
 define graphite::carbon::storage ( $pattern,$retentions){
   concat::fragment {$name:
-    target  => '/etc/carbon/storage-schemas.conf',
-    order   => 10,
-    content => template('graphite/storage-schemas.erb'),
-    notify  => Service['carbon-cache']
+    target   => '/etc/carbon/storage-schemas.conf',
+    order    => 10,
+    content  => template('graphite/storage-schemas.erb'),
+    requires => Package['carbon'],
+    notify   => Service['carbon-cache']
   }
 
 }
--- modules/wr/manifests/ala-lpd-puppet.pp
+++ modules/wr/manifests/ala-lpd-puppet.pp
@@ -7,8 +7,23 @@ class wr::ala-lpd-puppet {
   -> class { 'wr::master': }
   -> class { 'git::stomp_listener': }
   -> class { 'wr::puppetcommander': }
+  -> class { 'graphite': }
 
   Class['redhat'] -> class { 'nrpe': }
   class { 'nagios': }
   class { 'nagios::target': }
+
+  yumrepo {
+    'graphite':
+      baseurl  => 'http://ala-mirror.wrs.com/mirror/graphite',
+      descr    => 'Graphite',
+      gpgcheck => '0',
+  }
+  Yumrepo['graphite'] -> Class['graphite']
+
+  graphite::carbon::storage {
+    'default_10s_for_2weeks':
+      pattern    => '.*',
+      retentions => '10s:14d',
+  }
 }
