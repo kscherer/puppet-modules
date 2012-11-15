a40d4f2206bab66de4a4e73e24fedc15e3a1f2b1
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-11-15 13:28:16 -0500

    Hack around failure in ensure_resource

--- modules/yocto/manifests/init.pp
+++ modules/yocto/manifests/init.pp
@@ -3,7 +3,9 @@ class yocto {
 
   #workaround for lack of array support in ensure_resource
   define yocto::ensure_package() {
-    ensure_resource( 'package', $name, {'ensure' => 'installed' })
+    if ! defined(Package[$name]) {
+      ensure_resource( 'package', $name, {'ensure' => 'installed' })
+    }
   }
 
   $packages = hiera_array('packages')
