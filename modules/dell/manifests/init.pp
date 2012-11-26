385f88077abae49371be0769087151cf8530594b
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-11-26 10:45:28 -0500

    Install base OpenManage software on dell machines

--- modules/dell/manifests/init.pp
+++ modules/dell/manifests/init.pp
@@ -1,7 +1,10 @@
-#
+#Install the base OpenManage software for managing Dell Hardware
 class dell {
   $mirror = hiera('mirror')
   $dell_repo = '/etc/yum.repos.d/dell-omsa-repository.repo'
+
+  #Since dell repo is more complicated due to hardware model, etc.
+  #use the supplied bootstrap script to enable local mirror
   exec {
     'dell_repo':
       command => "wget -q -O - http://${mirror}/mirror/dell/hardware/latest/bootstrap.cgi | bash",
@@ -13,5 +16,22 @@ class dell {
     $dell_repo:
       ensure  => file,
       require => Exec['dell_repo'];
+  }
+
+  #package has to depend on file, not yum repo
+  package {
+    'srvadmin-base':
+      ensure  => latest,
+      require => File[$dell_repo];
+  }
+
+  #This service is necessary to run omreport cli tool
+  service {
+    'dataeng':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+      require    => Package['srvadmin-base'];
   }
 }
