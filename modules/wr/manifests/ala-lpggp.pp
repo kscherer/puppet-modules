5aa305c5bc8ed0ffb6220d1c73c615928bbbd506
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-12-18 16:01:48 -0500

    Open all ala-lpggp machines for general development

--- modules/wr/manifests/ala-lpggp.pp
+++ modules/wr/manifests/ala-lpggp.pp
@@ -37,33 +37,20 @@ class wr::ala-lpggp inherits wr::ala-common {
       'git-upload-archive','git-recieve-pack']:
   }
 
-  case $::hostname {
-    /ala-lpggp[2-3]/: {
-      motd::register{
-        'ala-lpggp':
-          content =>
-          'This machine is reserved for Linux Products automated testing only.';
-      }
-      # create a directory for exclusive use by lpg-test group
-      file {
-        [ "/${::hostname}1", "/${::hostname}2/"]:
-          ensure => 'directory',
-          group  => '3815',
-          mode   => '0775';
-      }
-    }
-    default: {
-      motd::register{
-        'ala-lpggp':
-          content => "This machine is for Linux Products developers manual compiles.
+  motd::register{
+    'ala-lpggp':
+      content => "This machine is for Linux Products developers manual compiles.
 It is not to be used for automated testing, automated builds or
 other uses. Please limit compiles to --enable-jobs=5.
-Use /${::hostname}1 as local storage.
+Use /${::hostname}[1-2] as local storage.
 It is not backed up, make sure you have a secure copy
 of your data.  Clean up after yourself, this F/S will be cleaned
 up periodically.";
-      }
-    }
   }
-
+  file {
+    [ "/${::hostname}1", "/${::hostname}2/"]:
+      ensure => 'directory',
+      group  => 'users',
+      mode   => '0777';
+  }
 }
