c95fa2b6e5799af73f4114c0d3ef6f2b9215f8cf
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-01-04 15:53:48 -0500

    Enable host tool builds on ala-lpggp1

--- modules/wr/manifests/ala-lpggp.pp
+++ modules/wr/manifests/ala-lpggp.pp
@@ -53,4 +53,84 @@ up periodically.";
       group  => 'users',
       mode   => '0777';
   }
+
+  if $::hostname == 'ala-lpggp1' {
+    #buildadmin user is a nis account, but without a nfs home directory.
+    file {
+      '/home/buildadmin':
+        ensure  => directory,
+        owner   => 'buildadmin',
+        group   => 'buildadmin',
+        require => Class['nis'],
+        mode    => '0755';
+      '/home/buildadmin/.ssh':
+        ensure => directory,
+        owner  => 'buildadmin',
+        group  => 'buildadmin',
+        mode   => '0700';
+    }
+
+    ssh_authorized_key {
+      'pkennedy_buildadmin':
+        ensure  => 'present',
+        user    => 'buildadmin',
+        key     => extlookup('pkennedy@linux-y9cs.site'),
+        require => File['/home/buildadmin/.ssh'],
+        type    => 'ssh-dss';
+      'wenzong_buildadmin':
+        ensure  => 'present',
+        user    => 'buildadmin',
+        key     => extlookup('wfan@pek-wenzong-fan'),
+        require => File['/home/buildadmin/.ssh'],
+        type    => 'ssh-dss';
+      'kscherer_windriver_buildadmin':
+        ensure  => 'present',
+        user    => 'buildadmin',
+        key     => extlookup('kscherer@yow-kscherer-l1'),
+        require => File['/home/buildadmin/.ssh'],
+        type    => 'ssh-dss';
+      'kscherer_home_buildadmin':
+        ensure  => 'present',
+        user    => 'buildadmin',
+        key     => extlookup('kscherer@helix'),
+        require => File['/home/buildadmin/.ssh'],
+        type    => 'ssh-rsa';
+    }
+
+    sudo::conf {
+      'admin':
+        source  => 'puppet:///modules/wr/sudoers.d/admin';
+    }
+
+    host {
+      'ala-lpgnas1-nfs':
+        ip           => '172.17.136.110',
+        host_aliases => 'ala-lpgnas1-nfs.wrs.com';
+      'ala-lpgnas2-nfs':
+        ip           => '172.17.136.114',
+        host_aliases => 'ala-lpgnas2-nfs.wrs.com';
+    }
+
+    file {
+      '/stored_builds':
+        ensure  => directory,
+        owner   => 'buildadmin',
+        group   => 'buildadmin',
+        require => Class['nis'];
+      '/buildarea':
+        ensure => link,
+        target => '/ala-lpggp12';
+    }
+
+    mount {
+      '/stored_builds':
+        ensure   => mounted,
+        atboot   => true,
+        device   => 'ala-lpgnas2-nfs:/vol/vol1',
+        fstype   => 'nfs',
+        options  => 'bg,vers=3,nointr,timeo=600,_netdev',
+        require  => File['/stored_builds'],
+        remounts => true;
+    }
+  }
 }
