9141e912871aff393decc268fee5e778c36d6486
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-01-23 12:40:33 -0500

    Remove collectd package is sufficient for disabling

--- modules/collectd/manifests/disable.pp
+++ modules/collectd/manifests/disable.pp
@@ -1,11 +1,7 @@
 #
 class collectd::disable {
+  #remove the package, which will disable the service as well
   package { 'collectd':
     ensure => absent,
-  }
-
-  service { 'collectd':
-    ensure => stopped,
-    enable => false,
   }
 }
