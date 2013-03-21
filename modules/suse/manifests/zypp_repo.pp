2bad3d8ff408a14baaeebabd1af374c1cdb4a921
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-03-21 11:17:46 -0400

    Enable autorefresh on zypper repos
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/suse/manifests/zypp_repo.pp
+++ modules/suse/manifests/zypp_repo.pp
@@ -3,11 +3,12 @@ define suse::zypp_repo( $baseurl, $enabled = '1' ){
 
   zypprepo {
     $name:
-      baseurl  => $baseurl,
-      descr    => $name,
-      enabled  => $enabled,
-      gpgcheck => '1',
-      gpgkey   => "${baseurl}/repodata/repomd.xml.key";
+      baseurl     => $baseurl,
+      descr       => $name,
+      enabled     => $enabled,
+      autorefresh => '1',
+      gpgcheck    => '1',
+      gpgkey      => "${baseurl}/repodata/repomd.xml.key";
   }
 
   #this is necessary to keep puppet from deleting the repo files
