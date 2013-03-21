d75c5779b02ed51564c76a750606e231c0efc647
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-03-21 11:15:31 -0400

    Move OpenSuSE vms to use new rsync mirror
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/suse/manifests/init.pp
+++ modules/suse/manifests/init.pp
@@ -1,4 +1,38 @@
 #
 class suse {
   ensure_resource( 'package', 'lsb-release', {'ensure' => 'installed' })
+
+  if $::operatingsystem == 'OpenSuSE' {
+
+    exec { 'zypper_refresh':
+      command     => '/usr/bin/zypper refresh',
+      refreshonly => true
+    }
+
+    #only puppet managed repo files in /etc/zypp/repos.d
+    file {
+      '/etc/zypp/repos.d/':
+        ensure  => directory,
+        recurse => true,
+        purge   => true,
+        owner   => root,
+        group   => root,
+        notify  => Exec['zypper_refresh'];
+    }
+
+    $mirror_host = hiera('mirror')
+    $mirror = "http://${mirror_host}/mirror"
+    $opensuse_dist = "${mirror}/opensuse/distribution/${::operatingsystemrelease}/repo/oss/suse"
+    $opensuse_updates = "${mirror}/opensuse/update/${::operatingsystemrelease}"
+    $opensuse_puppet = "${mirror}/opensuse/puppet/openSUSE_${::operatingsystemrelease}/"
+
+    zypp_repo {
+      'opensuse':
+        baseurl => $opensuse_dist;
+      'opensuse-updates':
+        baseurl => $opensuse_updates;
+      'opensuse-puppet':
+        baseurl => $opensuse_puppet;
+    }
+  }
 }
---
+++ modules/suse/manifests/zypp_repo.pp
@@ -0,0 +1,18 @@
+#eliminate common fields amoung repo definitions
+define suse::zypp_repo( $baseurl, $enabled = '1' ){
+
+  zypprepo {
+    $name:
+      baseurl  => $baseurl,
+      descr    => $name,
+      enabled  => $enabled,
+      gpgcheck => '1',
+      gpgkey   => "${baseurl}/repodata/repomd.xml.key";
+  }
+
+  #this is necessary to keep puppet from deleting the repo files
+  file {
+    "/etc/zypp/repos.d/${name}.repo":
+      ensure => file;
+  }
+}
