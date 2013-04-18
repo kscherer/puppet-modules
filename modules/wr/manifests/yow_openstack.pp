279c5a2e1c1aa1f3973f00d7c25955ae5206e702
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-04-18 10:47:00 -0400

    Add required parameters
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/yow_openstack.pp
+++ modules/wr/manifests/yow_openstack.pp
@@ -22,12 +22,17 @@ class wr::yow_openstack {
       floating_range       => '128.224.137.128/26',
       admin_email          => 'Konrad.Scherer@windriver.com',
       admin_password       => 'admin_password',
+      mysql_root_password  => 'mysql_pass',
+      keystone_db_password => 'keystone_db_password',
       keystone_admin_token => 'keystone_admin_token',
+      nova_db_password     => 'nova_db_password',
       nova_user_password   => 'nova_user_password',
       glance_user_password => 'glance_user_password',
+      glance_db_password   => 'glance_db_password',
       rabbit_password      => 'rabbit_password',
       rabbit_user          => 'openstack',
       libvirt_type         => 'kvm',
       fixed_range          => '10.0.0.0/24',
+      secret_key           => 'dummy_secret_key',
   }
 }
