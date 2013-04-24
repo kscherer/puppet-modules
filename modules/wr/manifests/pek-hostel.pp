3d1997b885bbb94ecbb6346cb48b3d9433368954
Wenzong Fan <wenzong.fan@windriver.com>
2013-04-24 09:35:55 +0800

    Update pek-hostel.pp
    Signed-off-by: Wenzong Fan <wenzong.fan@windriver.com>

--- modules/wr/manifests/pek-hostel.pp
+++ modules/wr/manifests/pek-hostel.pp
@@ -27,24 +27,39 @@ class wr::pek-hostel inherits wr::common {
       type   => 'ssh-dss';
   }
 
-  user {
-    'test':
-      ensure     => present,
-      managehome => true,
-      #sha-256 salted password windriver as requested by test team
-      password   => '$5$j5Wrmm4w$nsn03xSKYNSsUo1BmJqJZ3S1plhVZEMWzv7FajdZ7.B';
-  }
+  case $::hostname {
+    # userspace/yocto build hosts
+    /^pek-hostel-vm(0[1-9]|1[0-3]).*/: {
+      file {
+        '/buildarea':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0777';
+      }
+    }
 
-  group {
-    'test':
-  }
+    # host testing
+    default: {
+      user {
+        'test':
+          ensure     => present,
+          managehome => true,
+          #sha-256 salted password windriver as requested by test team
+          password   => '$5$j5Wrmm4w$nsn03xSKYNSsUo1BmJqJZ3S1plhVZEMWzv7FajdZ7.B';
+      }
 
-  file {
-    '/buildarea':
-      ensure => directory,
-      owner  => 'test',
-      group  => 'test',
-      mode   => '0755';
-  }
+      group {
+        'test':
+      }
 
+      file {
+        '/buildarea':
+          ensure => directory,
+          owner  => 'test',
+          group  => 'test',
+          mode   => '0755';
+      }
+    }
+  }
 }
