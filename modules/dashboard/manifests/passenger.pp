13ea0e233780fcb18cc290e367bf0080217e5847
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-05-29 15:39:40 -0400

    Update dashboard to use proper port value
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/dashboard/manifests/passenger.pp
+++ modules/dashboard/manifests/passenger.pp
@@ -13,20 +13,20 @@
 # Sample Usage:
 #
 class dashboard::passenger (
-  $dashboard_site,
-  $dashboard_port,
-  $passenger_ensure = undef,
+  $dashboard_site    = $dashboard::params::dashboard_site,
+  $dashboard_port    = $dashboard::params::dashboard_port,
+  $passenger_ensure  = undef,
   $passenger_package = undef,
 ) inherits dashboard {
 
   Package[$passenger_package]
-  -> Apache::Vhost["dashboard-$dashboard_site"]
+  -> Apache::Vhost["dashboard-${dashboard_site}"]
 
   file { '/etc/init.d/puppet-dashboard':
     ensure => absent,
   }
 
-  apache::vhost { "dashboard-$dashboard_site":
+  apache::vhost { "dashboard-${dashboard_site}":
     port        => $dashboard_port,
     priority    => '50',
     docroot     => '/usr/share/puppet-dashboard/public',
