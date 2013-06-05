cfbebb7ea7e26abcd1f0031b1863f9a3a6aaad18
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-06-05 15:42:34 -0400

    Remove dependency on extlookup
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/common/ssh_root_keys.pp
+++ modules/wr/manifests/common/ssh_root_keys.pp
@@ -4,22 +4,22 @@ class wr::common::ssh_root_keys {
     'kscherer_windriver':
       ensure => 'present',
       user   => 'root',
-      key    => extlookup('kscherer@yow-kscherer-l1'),
+      key    => hiera('kscherer@yow-kscherer-l1'),
       type   => 'ssh-dss';
     'kscherer_home':
       ensure => 'present',
       user   => 'root',
-      key    => extlookup('kscherer@helix'),
+      key    => hiera('kscherer@helix'),
       type   => 'ssh-rsa';
     'jch_laptop':
       ensure => 'present',
       user   => 'root',
-      key    => extlookup('jch@jch-schlepp.honig.net'),
+      key    => hiera('jch@jch-schlepp.honig.net'),
       type   => 'ssh-rsa';
     'jch_server':
       ensure => 'present',
       user   => 'root',
-      key    => extlookup('jch@kismet.honig.net'),
+      key    => hiera('jch@kismet.honig.net'),
       type   => 'ssh-rsa';
   }
 }
