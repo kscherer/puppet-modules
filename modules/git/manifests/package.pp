c7a1254882b24c30ee9e3828857622418fdbce8c
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-06-05 15:45:04 -0400

    Fix git::package class name
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/git/manifests/package.pp
+++ modules/git/manifests/package.pp
@@ -1,5 +1,5 @@
 #
-class git {
+class git::package {
   include git::params
   ensure_resource('package', $git::params::package_name, {'ensure' => 'installed' })
   ensure_resource('package', 'git-email',                {'ensure' => 'installed' })
