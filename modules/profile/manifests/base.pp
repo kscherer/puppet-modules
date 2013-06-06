57e5df81fb9be43a458ed55f4f147e0adafca0ae
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-06-06 09:50:36 -0400

    Make base profile smaller
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -2,28 +2,28 @@
 class profile::base {
   include motd
   include ntp
-  include nrpe
   include wr::common::repos
   include wr::common::ssh_root_keys
   include wr::common::etc_host_setup
   include wr::common::bash_configs
   include wr::workaround::xen
-  include wr::mcollective
   include puppet
 
   #make sure classes the may install packages come after repo info is updated
   Class['wr::common::repos'] -> Class['ntp']
-  Class['wr::common::repos'] -> Class['nrpe']
-  Class['wr::common::repos'] -> Class['wr::mcollective']
   Class['wr::common::repos'] -> Class['puppet']
 }
 
 #
 class profile::git::base {
+  include nrpe
+  include wr::mcollective
   include git::service
   include git::wr_bin_repo
   include git::grokmirror
   Class['wr::common::repos'] -> Class['git']
+  Class['wr::common::repos'] -> Class['nrpe']
+  Class['wr::common::repos'] -> Class['wr::mcollective']
 }
 
 #
