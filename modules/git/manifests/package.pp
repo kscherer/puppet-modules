396e51419b93a7aedf120129495db4d529dd9a9c
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-07-16 16:32:21 -0400

    Wrong facter var to identify all CentOS 5 machines
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/git/manifests/package.pp
+++ modules/git/manifests/package.pp
@@ -2,7 +2,7 @@
 class git::package {
   include git::params
 
-  if $::osfamily == 'RedHat' and $::lsbmajrelease == '5' {
+  if $::osfamily == 'RedHat' and $::lsbmajdistrelease == '5' {
     redhat::yum_repo {
       'git':
         baseurl => 'http://yow-mirror.wrs.com/mirror/git/5',
