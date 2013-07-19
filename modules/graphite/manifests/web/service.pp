0c29688442f6cac2d39e50c94da3c89cfe1ea0bd
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-07-19 12:25:51 -0400

    Use apache module to manage httpd and wsgi
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/graphite/manifests/web/service.pp
+++ modules/graphite/manifests/web/service.pp
@@ -1,11 +1,4 @@
 class graphite::web::service {
 
-  service { 'httpd':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    hasstatus  => true,
-  }
-
-
+  include apache::mod::wsgi
 }
