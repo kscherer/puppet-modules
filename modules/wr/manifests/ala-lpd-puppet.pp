adfe172e2863a2ddb92ab085f2e5491f30dd5357
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-09-04 15:30:38 -0400

    Add foreman fact uploader
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/ala-lpd-puppet.pp
+++ modules/wr/manifests/ala-lpd-puppet.pp
@@ -35,6 +35,8 @@ class wr::ala-lpd-puppet {
   $foreman_ssl_cert = hiera('foreman_ssl_cert')
   $foreman_ssl_key = hiera('foreman_ssl_key')
 
+  $inventory_upload = '/var/lib/puppet/foreman_upload_inventory.rb'
+
   file {
     '/usr/lib/ruby/site_ruby/1.8/puppet/reports/foreman.rb':
       ensure  => file,
@@ -42,5 +44,20 @@ class wr::ala-lpd-puppet {
       group   => 'puppet',
       mode    => '0644',
       content => template('wr/foreman.rb.erb');
+    $inventory_upload:
+      ensure  => file,
+      owner   => 'puppet',
+      group   => 'puppet',
+      mode    => '0755',
+      content => template('wr/foreman_upload_inventory.rb.erb');
+  }
+
+  cron {
+    'upload_inventory_to_foreman':
+      ensure  => present,
+      command => $inventory_upload,
+      user    => puppet,
+      minute  => '*/5',
+      require => File[$inventory_upload];
   }
 }
