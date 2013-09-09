57215f24b8d94d87de331583c19453503ba79ec0
Jeffrey C Honig <jch@honig.net>
2013-09-09 14:56:27 -0400

    Add packages needed for my scripts.

--- modules/wr/manifests/ala_lpgweb.pp
+++ modules/wr/manifests/ala_lpgweb.pp
@@ -1,4 +1,11 @@
 #
 class wr::ala_lpgweb {
   include profile::nis
+
+  #some packages needed to run perl CQ scripts
+  ensure_resource{'package', 'libdate-manip-perl', {'ensure' => 'latest'}}
+  ensure_resource{'package', 'liburi-perl', {'ensure' => 'latest'}}
+  ensure_resource{'package', 'libxml-simple-perl', {'ensure' => 'latest'}}
+  ensure_resource{'package', 'libxml-twig-perl', {'ensure' => 'latest'}}
+
 }
