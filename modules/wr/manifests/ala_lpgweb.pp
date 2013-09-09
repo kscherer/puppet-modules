18fba7dcb1cb5df50b5284efb920f32053d8476a
Jeffrey C Honig <jch@honig.net>
2013-09-09 15:05:57 -0400

    Syntax errors.

--- modules/wr/manifests/ala_lpgweb.pp
+++ modules/wr/manifests/ala_lpgweb.pp
@@ -3,12 +3,12 @@ class wr::ala_lpgweb {
   include profile::nis
 
   #some packages needed to run perl CQ scripts
-  ensure_resource{'package', 'libdate-manip-perl', {'ensure' => 'latest'}}
-  ensure_resource{'package', 'liburi-perl', {'ensure' => 'latest'}}
-  ensure_resource{'package', 'libxml-simple-perl', {'ensure' => 'latest'}}
-  ensure_resource{'package', 'libxml-twig-perl', {'ensure' => 'latest'}}
+  ensure_resource('package', 'libdate-manip-perl', {'ensure' => 'latest'})
+  ensure_resource('package', 'liburi-perl', {'ensure' => 'latest'})
+  ensure_resource('package', 'libxml-simple-perl', {'ensure' => 'latest'})
+  ensure_resource('package', 'libxml-twig-perl', {'ensure' => 'latest'})
 
   #some packages needed by jch's personal scripts
-  ensure_resource{'package', 'libnet-ldap-perl', {'ensure' => 'latest'}}
+  ensure_resource('package', 'libnet-ldap-perl', {'ensure' => 'latest'})
 
 }
