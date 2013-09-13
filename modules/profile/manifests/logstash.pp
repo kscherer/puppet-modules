3f1acc24e649b6204b3498b0ffe1a48b5a42b129
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-09-13 13:58:41 -0400

    Logstash test using file input
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/profile/manifests/logstash.pp
+++ modules/profile/manifests/logstash.pp
@@ -2,6 +2,12 @@
 class profile::logstash {
   include ::logstash
 
+  logstash::input::file {
+    'syslog':
+      type => 'syslog',
+      path => [ '/var/log/messages', '/var/log/syslog' ];
+  }
+
   logstash::output::elasticsearch {
     'elasticsearch':
       embedded => true
