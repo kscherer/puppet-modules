2b977aa2643bbd070a43b5d63dbe7a19d1bbb363
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-09-19 10:05:36 -0400

    Run smart on the hostel servers
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/xenserver.pp
+++ modules/wr/manifests/xenserver.pp
@@ -10,4 +10,12 @@ class wr::xenserver {
     'root':
       password => '$6$lWv2aSVT1/Yd$yVsrcIydMlcq4fkB23EDq6zZSmnR0Ab0NskE39YVODV9fcIl/MLLa4EplwSR4x/EqDX6O/H8Q7CwpLHUdEZpn0';
   }
+
+  #only run smart on the hostel builders
+  if $::blockdevice_sda_model == 'PERC 6/i' {
+    class {
+      'smart':
+        devices => {'/dev/sda'=>['megaraid,0', 'megaraid,1', 'megaraid,2', 'megaraid,3', 'megaraid,4', 'megaraid,5']};
+    }
+  }
 }
