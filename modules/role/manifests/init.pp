f448dcaec5840400d787f559860ab6b1c7ab4122
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-09-23 14:19:52 -0400

    Add basic monitoring to ala-git
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/role/manifests/init.pp
+++ modules/role/manifests/init.pp
@@ -8,6 +8,11 @@ class role::git::master inherits role {
   include git
   include nis
 
+  include nrpe
+  include nagios::target
+
+  Class['wr::common::repos'] -> Class['nrpe']
+  Class['wr::common::repos'] -> Class['nagios::target']
   Class['wr::common::repos'] -> Class['git']
   Class['wr::common::repos'] -> Class['nis']
 }
