f85adb8c425a92fc77851eab988575daca9f949c
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-09-25 11:55:07 -0400

    Setup cinder-volume on controller
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/yow_openstack.pp
+++ modules/wr/manifests/yow_openstack.pp
@@ -32,7 +32,8 @@ class wr::yow_openstack {
     }
 
     #controller has an lvm partition for vms as well
-    class { 'cinder::volume::iscsi': iscsi_ip_address => '127.0.0.1' }
+    include cinder::volume
+    #class { 'cinder::volume::iscsi': iscsi_ip_address => '127.0.0.1' }
 
     #setup cinder service on controller to use netapp
     cinder_config {
