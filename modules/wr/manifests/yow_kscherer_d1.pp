4486c4665912c7eedc4bf0bb89727fc11bdeb49a
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-10-08 09:24:57 -0400

    Add my new desktop
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

---
+++ modules/wr/manifests/yow_kscherer_d1.pp
@@ -0,0 +1,32 @@
+#
+class wr::yow_kscherer_d1 {
+  user {
+    'kscherer':
+      ensure => present,
+      uid    => '17298',
+      gid    => 'kscherer',
+      shell  => '/bin/bash',
+      home   => '/home/kscherer',
+      groups => ['users','adm'],
+  }
+
+  group {
+    'kscherer':
+      ensure => present,
+      git    => '1000',
+  }
+
+  package {
+    [ 'thunderbird','git','chromium-browser','konversation','emacs24','amarok',
+      'dmidecode', 'dos2unix', 'ethtool', 'flac', 'firefox', 'flashplugin-installer',
+      'fwknop-client', 'gnupg', 'puppet', 'keepassx', 'kpartx', 'konsole',
+      'latex-beamer', 'kvpnc', 'nfs-client', 'okular', 'openjdk-7-jre', 'parted',
+      'qemu-kvm', 'rake', 'vim-nox', 'git-email']:
+      ensure => latest;
+  }
+
+  include wr::common
+  include debian
+  include nis
+  include apt::unattended_upgrades
+}
