66422b8b135da60cba26c4d1486101903cb53d13
Scott Little <scott.little@windriver.com>
2013-10-22 17:12:26 -0400

    CGTS update

--- modules/wr/manifests/cgts.pp
+++ modules/wr/manifests/cgts.pp
@@ -13,4 +13,17 @@ class wr::cgts {
     'cgts':
       source  => 'puppet:///modules/wr/sudoers.d/cgts';
   }
+
+  package {
+    [ 'nedit', 'tightvncserver', 'gitk', 'konsole', 'motif-clients', 'ddd',
+      'openssh', 'compizconfig-settings-manager', 'gnome', 'kde-full', 'xfce4',
+      'ubuntu-desktop', 'openjdk-7-jre-lib', 
+      'icedtea-plugin', 'icedtea-7-plugin',
+      'eclipse-platform', 'codeblocks', 'doxygen',
+      'quilt', 'iotop', 
+      'nfs-client', 'okular', 'openjdk-7-jre', 'parted',
+      'qemu-kvm', 'rake', 'vim-nox', 'git-email']:
+      ensure => latest;
+  }
+
 }
