8bc2571df3ce57db2321e5eb03d38fca716b13ed
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-10-30 13:13:32 -0400

    Enable nagios and graphite server on new puppet master
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/yow-lpd-puppet2.pp
+++ modules/wr/manifests/yow-lpd-puppet2.pp
@@ -10,9 +10,32 @@ class wr::yow-lpd-puppet2 {
   include hiera
   include puppetdb
   include puppetdb::master::config
-  include git::stomp_listener
-
   Class['wr::common::repos'] -> Class['puppetdb']
-  Class['wr::common::repos'] -> Class['hiera']
+  Class['wr::common::repos'] -> Class['hiera'] -> Class['puppet']
+  Class['puppetdb'] -> Class['puppetdb::master::config']
+
+
+  include git::stomp_listener
   Class['wr::common::repos'] -> Class['git::stomp_listener']
+
+  include apache::mod::wsgi
+  include graphite
+  Class['apache::mod::wsgi'] -> Class['graphite']
+
+  include nagios
+  include nagios::nsca::server
+
+  graphite::carbon::storage {
+    'default_10s_for_2weeks':
+      pattern    => '.*',
+      retentions => '10s:14d',
+  }
+
+  #concat is another possible extension point
+  concat::fragment {
+    'cpu-aggregrate':
+      target  => '/etc/carbon/aggregation-rules.conf',
+      order   => 1,
+      source  => 'puppet:///modules/wr/cpu-aggregation.conf';
+  }
 }
