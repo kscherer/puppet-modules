9eb969a43511c5628424c6cf51e2ce9a8ca34240
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-11-12 11:28:13 -0500

    Add logrotate option for httpd logs on ala-git due to error_log overflowing /var
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/role/manifests/git/master.pp
+++ modules/role/manifests/git/master.pp
@@ -12,4 +12,20 @@ class role::git::master inherits role {
   Class['wr::common::repos'] -> Class['git']
   Class['wr::common::repos'] -> Class['nis']
   Class['wr::common::repos'] -> Class['profile::bare_metal']
+
+  #add logrotate entries for apache to keep logs from overflowing /var
+  include logrotate::base
+  logrotate::rule {
+    'httpd':
+      ensure        => 'present',
+      path          => '/var/log/httpd/*log',
+      rotate        => 7,
+      rotate_every  => 'day',
+      compress      => true,
+      missingok     => true,
+      ifempty       => false,
+      dateext       => true,
+      sharedscripts => true,
+      postrotate    => '/sbin/service httpd reload > /dev/null 2>/dev/null || true',
+  }
 }
