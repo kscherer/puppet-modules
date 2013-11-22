96d801cf7bc1a7cb87fa63deac03cc83867b909e
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-11-22 08:28:08 -0500

    Move provisioner role to its own manifest file
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

---
+++ modules/role/manifests/provisioner.pp
@@ -0,0 +1,13 @@
+#
+class role::provisioner {
+  include profile::nis
+
+  cron {
+    'delete_foreman_reports':
+      ensure  => present,
+      user    => 'root',
+      hour    => '0',
+      minute  => '0',
+      command => '/usr/sbin/foreman-rake reports:expire days=1 status=0; foreman-rake reports:expire days=7';
+  }
+}
