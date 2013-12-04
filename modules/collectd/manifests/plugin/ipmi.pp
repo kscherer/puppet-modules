6df8fd563035a5668125bc1e9578eb51df699159
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-12-04 13:56:37 -0500

    Only enable ipmi collectd plugin on bare metal
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/collectd/manifests/plugin/ipmi.pp
+++ modules/collectd/manifests/plugin/ipmi.pp
@@ -1,11 +1,17 @@
 #
 class collectd::plugin::ipmi(
-  $conf_file = "collectd/ipmi.conf",
+  $conf_file = 'collectd/ipmi.conf',
   ) {
-    file {
-      '/etc/collectd.d/ipmi.conf':
-        ensure  => present,
-        notify  => Service['collectd'],
-        content => template("${conf_file}.erb");
+    #only enable ipmi sensor testing on bare metal
+    $is_virtual_bool = any2bool($::is_virtual)
+    $is_bare_metal=($is_virtual_bool == false and $::manufacturer =~ /Dell/)
+
+    if $is_bare_metal {
+      file {
+        '/etc/collectd.d/ipmi.conf':
+          ensure  => present,
+          notify  => Service['collectd'],
+          content => template("${conf_file}.erb");
+      }
     }
   }
