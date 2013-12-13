b1db2a1608049549d7b91ecca127dd6f12e7afdb
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-12-13 09:55:20 -0500

    First attempt at managing yow dhcpd server using puppet
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/role/manifests/provisioner.pp
+++ modules/role/manifests/provisioner.pp
@@ -10,4 +10,22 @@ class role::provisioner {
       minute  => '0',
       command => '/usr/sbin/foreman-rake reports:expire days=1 status=0; /usr/sbin/foreman-rake reports:expire days=7';
   }
+
+  #test dhcp module in yow only for now
+  if $::hostname == 'yow-lpd-provisioner' {
+    include dhcp
+
+    concat::fragment { 'dhcp-conf-extra':
+      target  => "${dhcp::dhcp_dir}/dhcpd.conf",
+      content => 'omapi-port 7911;',
+      order   => 80,
+    }
+
+    dhcp::pool{ 'host-test-lab.wrs.com':
+      network => '128.224.194.0',
+      mask    => '255.255.255.0',
+      range   => '128.224.194.95 128.224.194.99',
+      gateway => '128.224.194.1',
+    }
+  }
 }
