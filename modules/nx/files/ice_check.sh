dfe580cc532e983b07cbe9a4e185ccbebc4dba5b
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-12-17 14:33:27 -0500

    Detect and ignore "file changed as we read it" build failures from hostel
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/nx/files/ice_check.sh
+++ modules/nx/files/ice_check.sh
@@ -5,7 +5,7 @@ FAIL=$1
 #DEFAULT_BRANCH=$3
 
 if [ -f $FAIL/00-wrbuild.log ]; then
-    grep -qF -e 'Segmentation fault' -e 'internal compiler error:' -e 'Your Makefile has been rebuilt' $FAIL/00-wrbuild.log 2>/dev/null
+    grep -qF -e 'Segmentation fault' -e 'internal compiler error:' -e 'Your Makefile has been rebuilt' -e 'file changed as we read it' $FAIL/00-wrbuild.log 2>/dev/null
     if [ $? = 0 ]; then
         #It looks like this build failed due to an ICE, either hardware or VM related
         #Do not clutter the nx fail repo with this build
