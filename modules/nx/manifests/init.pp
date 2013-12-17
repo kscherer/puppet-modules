2b75279e024699ca719872517e84538e0c9efdce
Konrad Scherer <Konrad.Scherer@windriver.com>
2013-12-17 14:39:04 -0500

    Move ice_check to nx configs repo
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/nx/manifests/init.pp
+++ modules/nx/manifests/init.pp
@@ -182,9 +182,14 @@ class nx {
   }
 
   nx::script {
-    [ 'hostel-fix-config','ice_check.sh','pull-multicore-layer','hostel-make',
+    [ 'hostel-fix-config','pull-multicore-layer','hostel-make',
       'pull-toolchain.sh','send_stats_to_graphite.sh',
       'sstate-cache-management.sh']:
+  }
+
+  file {
+    '/home/nxadm/bin/ice_check.sh':
+      ensure => absent;
   }
 
   case $::hostname {
