b5e3436842930184b0f51147d1d7132cd6f5e135
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-01-23 12:20:02 -0500

    Setup rq user on ala-lpgweb2 for all python-rq code
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

--- modules/wr/manifests/ala_lpgweb.pp
+++ modules/wr/manifests/ala_lpgweb.pp
@@ -19,4 +19,51 @@ class wr::ala_lpgweb {
 
   #setup redis for python-rq
   include redis
+
+  #Setup user for python-rq and python-rq-dashboard
+  user {
+    'rq':
+      ensure     => present,
+      groups     => 'rq',
+      home       => '/home/rq',
+      shell      => 'bin/bash',
+      managehome => true,
+      password   => '$6$YTdVKDWvBu6$6ptIEV1e5cAKCq8weSWXEMwJugEq/xzG0.WzNDSHTSk5QbQUJy4bDPYPwU1ZE8jBkGiOzPrUogwSvS1dBbyuU0';
+  }
+
+  group {
+    'rq':
+      ensure => present;
+  }
+
+  ssh_authorized_key {
+    'kscherer_desktop_rq':
+      ensure => 'present',
+      user   => 'rq',
+      key    => hiera('kscherer@yow-kscherer-d1'),
+      type   => 'ssh-rsa';
+    'kscherer_home_rq':
+      ensure => 'present',
+      user   => 'rq',
+      key    => hiera('kscherer@helix'),
+      type   => 'ssh-rsa';
+  }
+
+  file {
+    '/home/rq/.bashrc':
+      ensure => present,
+      owner  => 'rq',
+      group  => 'rq',
+      mode   => '0755',
+      source => 'puppet:///modules/wr/bashrc';
+    '/home/rq/.aliases':
+      ensure => present,
+      owner  => 'rq',
+      group  => 'rq',
+      mode   => '0755',
+      source => 'puppet:///modules/wr/aliases';
+    '/home/rq/.bash_profile':
+      ensure  => present,
+      content => 'if [ -f $HOME/.bashrc ]; then source $HOME/.bashrc; fi';
+  }
 }
