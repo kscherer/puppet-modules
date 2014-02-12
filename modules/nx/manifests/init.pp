757f16f0c6ced822d505293005bd45f1cd2b7086
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-02-12 15:15:59 -0500

    Change cronjob to use new cleanup-sstate-cache script

--- modules/nx/manifests/init.pp
+++ modules/nx/manifests/init.pp
@@ -230,7 +230,7 @@ class nx {
       require => User[ 'nxadm' ];
     #Clean out the sstate cache to avoid running out of disk
     'clean_sstate_cache':
-      command => "if [ -d ${sstate_dir} ]; then cd /home/nxadm/nx/${::hostname}.1/current_build/layers/oe-core/scripts; ./sstate-cache-management.sh --yes --remove-duplicated --cache-dir=${sstate_dir}; fi",
+      command => "if [ -d ${sstate_dir} ]; then cd /home/nxadm/bin; ./clean-sstate-cache.sh; fi",
       user    => nxadm,
       weekday => '*',
       hour    => 20,
