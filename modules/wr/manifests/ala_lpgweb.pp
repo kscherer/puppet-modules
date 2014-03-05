df1fc3b98094a3f3891efc303e09f161b3fe428e
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-05 15:07:45 -0500

    Create a user to hold the openembedded layer index web app

--- modules/wr/manifests/ala_lpgweb.pp
+++ modules/wr/manifests/ala_lpgweb.pp
@@ -20,74 +20,78 @@ class wr::ala_lpgweb {
   #setup redis for python-rq
   include redis
 
-  #Setup user for python-rq and python-rq-dashboard
-  user {
+  define webuser( $password ) {
+    user {
+      $name:
+        ensure     => present,
+        groups     => $name,
+        home       => "/home/${name}",
+        shell      => '/bin/bash',
+        managehome => true,
+        password   => $password;
+    }
+
+    group {
+      $name:
+        ensure => present;
+    }
+
+    ssh_authorized_key {
+      "kscherer_desktop_${name}":
+        ensure => 'present',
+        user   => $name,
+        key    => hiera('kscherer@yow-kscherer-d1'),
+        type   => 'ssh-rsa';
+      "kscherer_home_${name}":
+        ensure => 'present',
+        user   => $name,
+        key    => hiera('kscherer@helix'),
+        type   => 'ssh-rsa';
+    }
+
+    file {
+      "/home/${name}/.bashrc":
+        ensure => present,
+        owner  => $name,
+        group  => $name,
+        mode   => '0755',
+        source => 'puppet:///modules/wr/bashrc';
+      '/home/oelayer/.aliases':
+        ensure => present,
+        owner  => $name,
+        group  => $name,
+        mode   => '0755',
+        source => 'puppet:///modules/wr/aliases';
+      "/home/${name}/.bash_profile":
+        ensure  => present,
+        owner   => $name,
+        group   => $name,
+        content => 'if [ -f $HOME/.bashrc ]; then source $HOME/.bashrc; fi';
+    }
+  }
+
+  webuser {
     'rq':
-      ensure     => present,
-      groups     => 'rq',
-      home       => '/home/rq',
-      shell      => '/bin/bash',
-      managehome => true,
       password   => '$6$YTdVKDWvBu6$6ptIEV1e5cAKCq8weSWXEMwJugEq/xzG0.WzNDSHTSk5QbQUJy4bDPYPwU1ZE8jBkGiOzPrUogwSvS1dBbyuU0';
-  }
-
-  group {
-    'rq':
-      ensure => present;
-  }
-
-  ssh_authorized_key {
-    'kscherer_desktop_rq':
-      ensure => 'present',
-      user   => 'rq',
-      key    => hiera('kscherer@yow-kscherer-d1'),
-      type   => 'ssh-rsa';
-    'kscherer_home_rq':
-      ensure => 'present',
-      user   => 'rq',
-      key    => hiera('kscherer@helix'),
-      type   => 'ssh-rsa';
-  }
-
-  file {
-    '/home/rq/.bashrc':
-      ensure => present,
-      owner  => 'rq',
-      group  => 'rq',
-      mode   => '0755',
-      source => 'puppet:///modules/wr/bashrc';
-    '/home/rq/.aliases':
-      ensure => present,
-      owner  => 'rq',
-      group  => 'rq',
-      mode   => '0755',
-      source => 'puppet:///modules/wr/aliases';
-    '/home/rq/.bash_profile':
-      ensure  => present,
-      owner   => 'rq',
-      group   => 'rq',
-      content => 'if [ -f $HOME/.bashrc ]; then source $HOME/.bashrc; fi';
   }
 
   #This installs python-pip
   include python
 
-  python::pip {
-    'rq':
-      ensure       => present,
-      owner        => 'rq',
-      environment  => 'HOME=/home/rq',
-      install_args => '--user --build=/home/rq/.pip/build';
-    'rq-dashboard':
-      ensure       => present,
-      owner        => 'rq',
-      environment  => 'HOME=/home/rq',
-      install_args => '--user --build=/home/rq/.pip/build';
-    'jira-python':
-      ensure       => present,
-      owner        => 'rq',
-      environment  => 'HOME=/home/rq',
-      install_args => '--user --build=/home/rq/.pip/build';
+  #define to reduce boilerplate
+  define pip_package ( $owner ) {
+    python::pip {
+      $name:
+        ensure       => present,
+        owner        => $owner,
+        environment  => "HOME=/home/${owner}",
+        install_args => "--user --build=/home/${owner}/.pip/build";
+    }
+  }
+
+  pip_package {
+    ['rq', 'rq-dashboard', 'jira-python' ]:
+      owner => 'rq';
   }
 
   #Setup supervisord to monitor worker process
@@ -147,4 +151,27 @@ class wr::ala_lpgweb {
 
   #plantuml requires java
   include java
+
+  #setup for open embedded layer index web app
+  webuser {
+    'oelayer':
+      password => '$6$JE9EdCoGh$SgIhKWioQFsTrbTLZ05MMXXxfV/err6ZNksOSSluUhg6irGg.9b52fo0R.ydaXOVS8GcKWoU2Z6wm.FFjWcYv0';
+  }
+
+  pip_package {
+    ['django', 'South', 'django-registration', 'django-reversion',
+      'django-reversion-compare', 'django-simple-captcha', 'django-nvd3',
+      'GitPython']:
+      owner => 'oelayer';
+  }
+
+  vcsrepo {
+    'openembedded_layerindex':
+      ensure   => 'present',
+      path     => '/home/oelayer/layerindex-web',
+      provider => 'git',
+      source   => 'git://git.yoctoproject.org/layerindex-web',
+      user     => 'oelayer',
+      revision => 'master';
+  }
 }
