69cce76c71d1402d2ab338544c856a13e7d0f58c
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-12 15:59:41 -0400

    Add docker registry to ala-lpd-mesos

--- modules/wr/manifests/ala_lpd_mesos.pp
+++ modules/wr/manifests/ala_lpd_mesos.pp
@@ -1,6 +1,13 @@
+#System responsible for mesos master, chronus, zookeeper and private
+#docker registry
 class wr::ala_lpd_mesos {
   include profile::nis
   include ssmtp
   include docker
   include collectd
+
+  docker::image {
+    'registry':
+      image_tag => 'latest';
+  }
 }
