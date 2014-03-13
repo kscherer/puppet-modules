3e9adefd657f6d4c836783cfcbd21ea3d4d9a5bd
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-13 14:12:03 -0400

    Setup and run the docker registry

--- modules/wr/manifests/ala_lpd_mesos.pp
+++ modules/wr/manifests/ala_lpd_mesos.pp
@@ -6,8 +6,30 @@ class wr::ala_lpd_mesos {
   include docker
   include collectd
 
+  #setup for the docker registry
   docker::image {
     'registry':
       image_tag => 'latest';
+  }
+
+  #store all registry info in /opt
+  file {
+    '/opt/registry':
+      ensure => directory;
+    '/opt/registry/store':
+      ensure => directory;
+    '/opt/registry/config.yml':
+      ensure => present,
+      source => 'puppet:///modules/wr/docker_registry_config.yml';
+  }
+
+  #Run the registry image with bind mount to registry directory and config
+  docker::run {
+    'registry':
+      image   => 'registry',
+      port    => ['5000','5000'],
+      volumes => ['/opt/registry','registry'],
+      env     => ['DOCKER_REGISTRY_CONFIG=/registry/config.yml'],
+      require => [ File['/opt/registry/store'], File['/opt/registry/config.yml']];
   }
 }
