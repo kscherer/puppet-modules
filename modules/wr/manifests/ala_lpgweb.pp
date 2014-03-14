6a1f719a046269f572544560a1a8c69be5aabe01
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-14 11:04:19 -0400

    Puppetize apache gunicorn redirect config

--- modules/wr/manifests/ala_lpgweb.pp
+++ modules/wr/manifests/ala_lpgweb.pp
@@ -80,6 +80,8 @@ class wr::ala_lpgweb {
     'ala-lpgweb2.wrs.com':
       port       => 80,
       docroot    => '/var/www/',
+      aliases    => [ { alias => '/static/',
+                        path  => '/home/oelayer/layerindex-web/layerindex/static/' } ],
       proxy_pass => [ { 'path' => '/layerindex',
                         'url'  => 'http://127.0.0.1:8001/layerindex' }];
   }
