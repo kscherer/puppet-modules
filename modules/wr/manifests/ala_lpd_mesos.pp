c17c6082f7f2fa2e3f77f72130e0d9829d09a587
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-26 10:16:11 -0400

    Workaround a bug in docker registry container by updating to latest

--- modules/wr/manifests/ala_lpd_mesos.pp
+++ modules/wr/manifests/ala_lpd_mesos.pp
@@ -24,13 +24,15 @@ class wr::ala_lpd_mesos {
   }
 
   #Run the registry image with bind mount to registry directory and config
+  #As of Mar 26, docker index was out of date and I added the commands to
+  #update the git repo inside the image
   docker::run {
     'registry':
       image   => 'registry',
-      command => ' ',
+      command => 'cd /docker-registry && git remote add origin https://github.com/dotcloud/docker-registry.git && git pull origin master && ./setup-configs.sh && exec ./run.sh',
       ports   => ['5000:5000'],
       volumes => ['/opt/registry:/registry'],
-      env     => ['DOCKER_REGISTRY_CONFIG=/registry/config.yml'],
+      env     => ['DOCKER_REGISTRY_CONFIG=/registry/config.yml','SETTINGS_FLAVOR=prod'],
       require => [ File['/opt/registry/store'], File['/opt/registry/config.yml']];
   }
 }
