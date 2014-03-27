05feb14edad1fc59a782508ce7cd608ad69f370d
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-27 10:36:54 -0400

    Workarounds to get mesos slave working

--- modules/profile/manifests/mesos_slave.pp
+++ modules/profile/manifests/mesos_slave.pp
@@ -1,4 +1,10 @@
 #
 class profile::mesos_slave inherits profile::monitored {
   include mesos::slave
+
+  file {
+    '/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server/libjvm.so':
+      ensure => link,
+      target => '/usr/lib/libjvm.so';
+  }
 }
