586ad63143297875af206961f6da8dc193f024ed
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-27 14:00:13 -0400

    Install docker on mesos slaves

--- modules/profile/manifests/mesos_slave.pp
+++ modules/profile/manifests/mesos_slave.pp
@@ -1,6 +1,14 @@
 #
 class profile::mesos_slave inherits profile::monitored {
   include mesos::slave
+  include docker
+
+  docker::image {
+    'ala-lpd-mesos.wrs.com:5000/centos5_32':
+      image_tag => 'wrl';
+    'ala-lpd-mesos.wrs.com:5000/centos5_64':
+      image_tag => 'wrl';
+  }
 
   file {
     '/usr/lib/libjvm.so':
