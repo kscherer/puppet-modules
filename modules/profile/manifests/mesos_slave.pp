5e8876034167c54245650907117bdfaee7bcff51
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-03-27 10:38:37 -0400

    One of these days I will remember which way to do symlinks

--- modules/profile/manifests/mesos_slave.pp
+++ modules/profile/manifests/mesos_slave.pp
@@ -3,8 +3,8 @@ class profile::mesos_slave inherits profile::monitored {
   include mesos::slave
 
   file {
-    '/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server/libjvm.so':
+    '/usr/lib/libjvm.so':
       ensure => link,
-      target => '/usr/lib/libjvm.so';
+      target => '/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server/libjvm.so';
   }
 }
