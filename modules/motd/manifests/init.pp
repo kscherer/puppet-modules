ecc697d72a8c5af4f84e141375d098d265cc463b
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-04-04 15:09:51 -0400

    file mode must be quoted

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -6,15 +6,17 @@ class motd {
     default  : { $motd = '/etc/motd' }
   }
 
-  concat{$motd:
-    owner => root,
-    group => root,
-    mode  => 644
+  concat{
+    $motd:
+      owner => root,
+      group => root,
+      mode  => '0644';
   }
 
-  concat::fragment{'motd_header':
-    target  => $motd,
-    content => template('motd/motd.erb'),
-    order   => 01,
+  concat::fragment{
+    'motd_header':
+      target  => $motd,
+      content => template('motd/motd.erb'),
+      order   => 01;
   }
 }
