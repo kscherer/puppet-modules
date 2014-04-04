ba817a5cca71eb6860cd46ec85e25d13d4bfad12
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-04-04 14:55:33 -0400

    Remove extra spaces

--- modules/wr/manifests/yow-buildbot-slave.pp
+++ modules/wr/manifests/yow-buildbot-slave.pp
@@ -9,8 +9,7 @@ class wr::yow-buildbot-slave inherits wr::mcollective {
 
   user {
     'root':
-      password =>
-        '$6$vEbdCo3ZF/z2cq9$MawClitGcpDau8b26qI.r/VRFCmJM.MrA41vRimQr69.XPgOac7A.Vc7oNoNb4wELuUtWzcDtWlmsNKbnxXSX0';
+      password => '$6$vEbdCo3ZF/z2cq9$MawClitGcpDau8b26qI.r/VRFCmJM.MrA41vRimQr69.XPgOac7A.Vc7oNoNb4wELuUtWzcDtWlmsNKbnxXSX0';
   }
 
   motd::register{
