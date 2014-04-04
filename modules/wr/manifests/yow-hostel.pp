6e0bff1231fd1ac10645185b22729b7143ef5189
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-04-04 14:29:39 -0400

    Parser is complaining

--- modules/wr/manifests/yow-hostel.pp
+++ modules/wr/manifests/yow-hostel.pp
@@ -5,8 +5,7 @@ class wr::yow-hostel {
 
   user {
     'root':
-      password =>
-        '$6$vEbdCo3ZF/z2cq9$MawClitGcpDau8b26qI.r/VRFCmJM.MrA41vRimQr69.XPgOac7A.Vc7oNoNb4wELuUtWzcDtWlmsNKbnxXSX0';
+      password => '$6$vEbdCo3ZF/z2cq9$MawClitGcpDau8b26qI.r/VRFCmJM.MrA41vRimQr69.XPgOac7A.Vc7oNoNb4wELuUtWzcDtWlmsNKbnxXSX0';
   }
 
   motd::register{
