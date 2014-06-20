fe4b5145f2a673fb7509cdf2df86deef37cb7281
Eman Jado <eman.jado@windriver.com>
2014-06-20 15:21:01 -0400

    Update yow_osc with reqired services and packages
    Signed-off-by: Eman Jado <eman.jado@windriver.com>

--- modules/wr/manifests/yow_osc.pp
+++ modules/wr/manifests/yow_osc.pp
@@ -1,3 +1,15 @@
 #
 class wr::yow_osc {
+  include nis
+  include apache
+  include ntp
+
+  package {
+    [ 'tzdata-java','openjdk-7-jdk','openjdk-7-jre','openjdk-7-jre-headless',
+      'python26', 'mrepo', 'python-ssl', 'mirrormanager-client', 'db4-devel',
+      'libstdc++6:i386','libgtk2.0-0:i386','libxtst6:i386','bc',
+      'vim','vim-runtime','xutils-dev','expect']:
+      ensure => 'installed';
+  }
+
 }
