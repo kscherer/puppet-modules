51e081e12b0f50c05761a2d324522bc3ac59bbee
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-07-17 16:37:49 -0400

    Monitoring disabled on RH7 for now

--- modules/profile/manifests/monitored.pp
+++ modules/profile/manifests/monitored.pp
@@ -1,15 +1,18 @@
 #
 class profile::monitored inherits profile::base {
-  include nrpe
+
   include wr::mcollective
-  include nagios::target
+  Class['wr::common::repos'] -> Class['wr::mcollective']
 
   $is_virtual_bool = any2bool($::is_virtual)
   if $is_virtual_bool == false and $::manufacturer =~ /Dell/ and $::productname !~ /(OptiPlex|Precision)/ {
     include profile::bare_metal
   }
 
-  Class['wr::common::repos'] -> Class['nrpe']
-  Class['wr::common::repos'] -> Class['wr::mcollective']
-  Class['wr::common::repos'] -> Class['nagios::target']
+  if ($::osfamily == 'RedHat' and $::lsbmajdistrelease != '7') {
+    include nrpe
+    include nagios::target
+    Class['wr::common::repos'] -> Class['nrpe']
+    Class['wr::common::repos'] -> Class['nagios::target']
+  }
 }
