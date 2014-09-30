5c0eff3e95c99377cb6f38f38800a945235ef4aa
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-09-30 08:44:48 -0400

    Update to 0.20.1 and add wrlbuild user to mesos master

--- modules/profile/manifests/mesos/common.pp
+++ modules/profile/manifests/mesos/common.pp
@@ -1,6 +1,52 @@
 #
 class profile::mesos::common inherits profile::nis {
 
+  include docker
+  Class['wr::common::repos'] -> Class['docker']
+
+  #Use collectd to monitor system utilization
+  include collectd
+  Class['wr::common::repos'] -> Class['collectd']
+
+  # Do builds as an unprivileged user which matches uid of user in docker
+  group {
+    'wrlbuild':
+      ensure => present,
+  }
+
+  user {
+    'wrlbuild':
+      ensure     => present,
+      gid        => 'wrlbuild',
+      uid        => 1000,
+      managehome => true,
+      home       => '/home/wrlbuild',
+      groups     => ['docker'],
+      shell      => '/bin/bash',
+      password   => '$5$6F1BpKqFcszWi0n$fC5yUBkPNXHfyL8TOJwdJ1EE8kIzwJnKVrtcFYnpbcA',
+      require    => Group [ 'wrlbuild' ];
+  }
+
+  # group created by docker package, must be declared here so that wrlbuild
+  # can be in docker group
+  group {
+    'docker':
+      ensure  => present,
+      require => Class['docker'];
+  }
+
+  #turn off locate package which scans filesystem and use a lot of IO
+  ensure_resource('package', 'mlocate', {'ensure' => 'absent' })
+
+  vcsrepo {
+    '/home/wrlbuild/wr-buildscripts':
+      ensure   => 'present',
+      provider => 'git',
+      source   => 'git://ala-git.wrs.com/lpd-ops/wr-buildscripts.git',
+      user     => 'wrlbuild',
+      revision => 'master';
+  }
+
   file {
     '/usr/lib/libjvm.so':
       ensure => link,
@@ -33,7 +79,7 @@ class profile::mesos::common inherits profile::nis {
   Apt::Source['mesos'] -> Package['mesos']
 
   #install mesos egg
-  $mesos_egg = 'mesos-0.20.0-py2.7-linux-x86_64.egg'
+  $mesos_egg = 'mesos-0.20.1-py2.7-linux-x86_64.egg'
 
   exec {
     'mesos_egg':
@@ -44,5 +90,18 @@ class profile::mesos::common inherits profile::nis {
       command => "/usr/bin/easy_install /root/${mesos_egg}",
       unless  => "/usr/bin/test -e /usr/local/lib/python2.7/dist-packages/${mesos_egg}",
       require => [ Package['python-setuptools'], Exec['mesos_egg']];
+  }
+
+  #Use hosts file as substitute for geographically aware DNS
+  $registry_ip = $::location ? {
+    'yow'   => '128.224.194.16', #yow-lpd-provision
+    default => '147.11.106.56' #ala-lpd-mesos
+  }
+
+  host {
+    'wr-docker-registry':
+      ensure       => present,
+      ip           => $registry_ip,
+      host_aliases => 'wr-docker-registry.wrs.com';
   }
 }
--- modules/profile/manifests/mesos/slave.pp
+++ modules/profile/manifests/mesos/slave.pp
@@ -1,12 +1,6 @@
 #
 class profile::mesos::slave inherits profile::mesos::common {
   include ::mesos::slave
-  include docker
-  Class['wr::common::repos'] -> Class['docker']
-
-  #Use collectd to monitor system utilization
-  include collectd
-  Class['wr::common::repos'] -> Class['collectd']
 
   #The mesos package can start the mesos master so make sure it is
   #not running on slaves
@@ -15,43 +9,6 @@ class profile::mesos::slave inherits profile::mesos::common {
       ensure => stopped;
   }
   Package['mesos']->Service['mesos-master']
-
-  # Do builds as an unprivileged user which matches uid of user in docker
-  group {
-    'wrlbuild':
-      ensure => present,
-  }
-
-  user {
-    'wrlbuild':
-      ensure     => present,
-      gid        => 'wrlbuild',
-      uid        => 1000,
-      managehome => true,
-      home       => '/home/wrlbuild',
-      groups     => ['docker'],
-      shell      => '/bin/bash',
-      password   => '$5$6F1BpKqFcszWi0n$fC5yUBkPNXHfyL8TOJwdJ1EE8kIzwJnKVrtcFYnpbcA',
-      require    => Group [ 'wrlbuild' ];
-  }
-
-  group {
-    'docker':
-      ensure  => present,
-      require => Class['docker'];
-  }
-
-  #turn off locate package which scans filesystem and use a lot of IO
-  ensure_resource('package', 'mlocate', {'ensure' => 'absent' })
-
-  vcsrepo {
-    '/home/wrlbuild/wr-buildscripts':
-      ensure   => 'present',
-      provider => 'git',
-      source   => 'git://ala-git.wrs.com/lpd-ops/wr-buildscripts.git',
-      user     => 'wrlbuild',
-      revision => 'master';
-  }
 
   file {
     ['/home/wrlbuild/log','/home/wrlbuild/builds']:
