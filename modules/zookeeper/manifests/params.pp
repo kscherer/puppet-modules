17bda2422c5adefe8b66375f9771d9720124a5b5
Konrad Scherer <Konrad.Scherer@windriver.com>
2014-10-30 16:34:26 -0400

    Parmeterize name of cron package

--- modules/zookeeper/manifests/params.pp
+++ modules/zookeeper/manifests/params.pp
@@ -5,11 +5,13 @@ class zookeeper::params{
       $zookeeper_package = 'zookeeper'
       $zookeeperd_package = 'zookeeperd'
       $zookeeper_service = 'zookeeper'
+      $cron_package      = 'cron'
     }
     'RedHat': {
       $zookeeper_package = 'zookeeper'
       $zookeeperd_package = 'zookeeper-server'
       $zookeeper_service = 'zookeeper-server'
+      $cron_package      = 'cronie'
     }
     default: {
       warning("Operating system ${::operatingsystem} not supported.")
