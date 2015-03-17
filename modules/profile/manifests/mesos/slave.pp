53f345da5d60eeb45b0c15dfa1cc1622c1b7fa21
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-03-17 12:55:18 -0400

    Add cron job to kill any container running longer than 1 day

--- modules/profile/manifests/mesos/slave.pp
+++ modules/profile/manifests/mesos/slave.pp
@@ -67,6 +67,10 @@ class profile::mesos::slave inherits profile::mesos::common {
       user    => 'wrlbuild',
       hour    => '*',
       minute  => fqdn_rand(60, 'native_sstate_update');
+    'kill_hung_builds':
+      command => 'docker ps -a | grep \'days ago\' | awk \'{print $1}\' | xargs docker rm -f',
+      user    => 'root',
+      hour    => '0';
   }
 
   exec {
