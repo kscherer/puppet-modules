6656ade68ef98c3610856e9f63da8036a40a85e8
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-03-17 10:29:24 -0400

    Fix motd on Ubuntu systems

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,9 +1,5 @@
 # class to setup basic motd, include on all nodes
 class motd {
-  case $::operatingsystem {
-    'Debian' : { $motd = '/etc/motd.tail' }
-    default  : { $motd = '/etc/motd' }
-  }
 
   #Ubuntu uses a special update-motd script
   if $::operatingsystem == 'Ubuntu' {
@@ -16,20 +12,27 @@ class motd {
         content => "#!/bin/sh\ncat ${motd}";
       '/etc/update-motd.d/50windriver':
         ensure  => absent;
+      '/etc/motd':
+        ensure => link,
+        target => '/var/run/motd.dynamic';
     }
-  }
+  } else {
+    case $::operatingsystem {
+      'Debian' : { $motd = '/etc/motd.tail' }
+      default  : { $motd = '/etc/motd' }
+    }
+    concat{
+      $motd:
+        owner => root,
+        group => root,
+        mode  => '0644';
+    }
 
-  concat{
-    $motd:
-      owner => root,
-      group => root,
-      mode  => '0644';
-  }
-
-  concat::fragment{
-    'motd_header':
-      target  => $motd,
-      content => template('motd/motd.erb'),
-      order   => '01';
+    concat::fragment{
+      'motd_header':
+        target  => $motd,
+        content => template('motd/motd.erb'),
+        order   => '01';
+    }
   }
 }
