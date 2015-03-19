150b13301acf3a031007eaf0870ae6b100b529f0
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-03-19 11:57:22 -0400

    Another attempt to fix the motd on Ubuntu

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -2,34 +2,28 @@
 class motd {
   case $::operatingsystem {
     'Debian' : { $motd = '/etc/motd.tail' }
+    'Ubuntu' : { $motd = '/etc/update-motd.d/55-windriver' }
     default  : { $motd = '/etc/motd' }
   }
 
   #Ubuntu uses a special update-motd script
   if $::operatingsystem == 'Ubuntu' {
     file {
-      '/etc/update-motd.d/55-windriver':
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => '0755',
-        content => template('motd/motd.erb');
       '/etc/motd':
         ensure => absent;
     }
-  } else {
-    concat{
-      $motd:
-        owner => root,
-        group => root,
-        mode  => '0644';
-    }
+  }
+  concat{
+    $motd:
+      owner => root,
+      group => root,
+      mode  => '0644';
+  }
 
-    concat::fragment{
-      'motd_header':
-        target  => $motd,
-        content => template('motd/motd.erb'),
-        order   => '01';
-    }
+  concat::fragment{
+    'motd_header':
+      target  => $motd,
+      content => template('motd/motd.erb'),
+      order   => '01';
   }
 }
