bd647a223c4228c46d4ec605184cf71c6fe18b26
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-03-26 16:30:59 -0400

    On Fedora, /sys/fs/cgroup is a ro mounted tmpfs

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -220,6 +220,6 @@ class nrpe {
       parameters => '--nagios';
     'check_ro_mounts':
       command    => 'check_ro_mounts',
-      parameters => '-X nfs -X proc -X cgroup -X fuse -x "/mnt/*"';
+      parameters => '-X nfs -X proc -X cgroup -X fuse -X tmpfs -x "/mnt/*"';
   }
 }
