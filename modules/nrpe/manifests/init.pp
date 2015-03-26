81fb816ae9540e70aa56b9a69193ed757e074b8b
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-03-26 15:25:02 -0400

    Suse systems have a special ro proc mount for ntp
    This causes the ro mount script to report critical and this should be ignored

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -220,6 +220,6 @@ class nrpe {
       parameters => '--nagios';
     'check_ro_mounts':
       command    => 'check_ro_mounts',
-      parameters => '-X nfs -x "/mnt/*"';
+      parameters => '-X nfs -X proc -x "/mnt/*"';
   }
 }
