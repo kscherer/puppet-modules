becae48d190fa02aa07416831b8800d0dfca2c3a
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-06-19 12:06:28 -0400

    The define needs to be in its own file for the autoloader to work

---
+++ modules/wr/manifests/extract_key.pp
@@ -0,0 +1,12 @@
+# The name is a hash which is really wierd.
+# TODO: rewrite using future parser
+define wr::extract_key($user) {
+  $key=split($name["sshpubkey_${user}"],' ')
+  ssh_authorized_key {
+    $key[2]:
+      ensure => present,
+      type   => $key[0],
+      key    => $key[1],
+      user   => $user;
+  }
+}
