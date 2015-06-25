91e39e729059656ecc2cc470622d3a125569a3f2
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-06-25 15:42:37 -0400

    Override functions in sshkeys to stop traversing all nis users
    Signed-off-by: Konrad Scherer <Konrad.Scherer@windriver.com>

---
+++ modules/wr/lib/facter/homedir.rb
@@ -0,0 +1,36 @@
+require 'etc'
+
+# First get a list of users via the homedir_users fact
+#
+# An example of this file looks like:
+# $ cat /etc/facter/facts.d/homedir_users.yaml
+# ---
+# homedir_users:
+#   - root
+#   - jdoe
+#
+# Missing users will be skipped
+#
+# If this fact does not exist, all users in /etc/passwd
+# will be obtained.
+#
+users = {}
+homedir_users = Facter.value(:homedir_users)
+if homedir_users != nil and homedir_users.size > 0
+  homedir_users.each do |u|
+    begin
+      user = Etc.getpwnam(u)
+      users[user.name] = user.dir
+    rescue
+      next
+    end
+  end
+end
+
+users.each do |user_name, user_dir|
+   Facter.add("home_#{user_name}") do
+      setcode do
+         user_dir
+      end
+   end
+end
