6b5a33cfaa4264e5d6c6fabc0f4b57e67fcb31a3
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-10-08 19:47:47 -0400

    Another dell yum repo that doesn't work on RH5

--- modules/dell/manifests/hwtools.pp
+++ modules/dell/manifests/hwtools.pp
@@ -37,26 +37,29 @@ class dell::hwtools {
         mode   => '0644',
       }
 
-      # http://linux.dell.com/wiki/index.php/Repository/software
-      yumrepo {'dell-omsa-indep':
-        descr      => 'Dell OMSA repository - Hardware independent',
-        mirrorlist => "${dell::omsa_url_base}${dell::omsa_version}/mirrors.cgi?${dell::omsa_url_args_indep}",
-        enabled    => '1',
-        gpgcheck   => '1',
-        gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
-        require    => [
-          File['/etc/pki/rpm-gpg/RPM-GPG-KEY-dell'],
-          File['/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios'],
-        ]
-      }
+      # don't manage openmanage yum repos on RH5 because upstream is broken
+      if $::operatingsystemmajrelease != '5' {
+        # http://linux.dell.com/wiki/index.php/Repository/software
+        yumrepo {'dell-omsa-indep':
+          descr      => 'Dell OMSA repository - Hardware independent',
+          mirrorlist => "${dell::omsa_url_base}${dell::omsa_version}/mirrors.cgi?${dell::omsa_url_args_indep}",
+          enabled    => '1',
+          gpgcheck   => '1',
+          gpgkey     => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-dell\n\tfile:///etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios",
+          require    => [
+                         File['/etc/pki/rpm-gpg/RPM-GPG-KEY-dell'],
+                         File['/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios'],
+                         ]
+        }
 
-      # ensure file is managed in case we want to purge /etc/yum.repos.d/
-      # http://projects.puppetlabs.com/issues/3152
-      file { '/etc/yum.repos.d/dell-omsa-indep.repo':
-        ensure  => present,
-        mode    => '0644',
-        owner   => 'root',
-        require => Yumrepo['dell-omsa-indep'],
+        # ensure file is managed in case we want to purge /etc/yum.repos.d/
+        # http://projects.puppetlabs.com/issues/3152
+        file { '/etc/yum.repos.d/dell-omsa-indep.repo':
+          ensure  => present,
+          mode    => '0644',
+          owner   => 'root',
+          require => Yumrepo['dell-omsa-indep'],
+        }
       }
 
       file { '/etc/yum.repos.d/dell-software-repo.repo':
