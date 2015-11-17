e49d65dd30507cf73babc6964007f4d589534155
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-11-17 13:23:40 -0500

    Make sure /opt/consul gets created before installation

--- modules/profile/manifests/consul.pp
+++ modules/profile/manifests/consul.pp
@@ -1,6 +1,16 @@
 #
 class profile::consul {
 
+  file {
+    '/opt/consul':
+      ensure => directory,
+      owner => 'consul',
+      group => 'consul',
+      mode  => '0755';
+  }
+
+  File['/opt/consul'] -> Class['::consul']
+
   class {
     '::consul':
       config_hash => hiera_hash('consul_config_hash');
