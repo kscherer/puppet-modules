54b8932a576ef04976c46fa800e3df9d86232971
Konrad Scherer <Konrad.Scherer@windriver.com>
2015-11-23 12:23:21 -0500

    For consul service on Debian 8 to be systemd

--- modules/profile/manifests/consul.pp
+++ modules/profile/manifests/consul.pp
@@ -6,6 +6,11 @@ class profile::consul {
       ensure => directory;
   }
 
+  # Puppet is not detecting systemd on Debian 8 properly
+  if $::operatingsystem == 'Debian' {
+    Service {provider => 'systemd'}
+  }
+
   class {
     '::consul':
       config_hash => hiera_hash('consul_config_hash');
