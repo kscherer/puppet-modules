9e464b96b059193546e2516ddc4a6fcb2c7455bd
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-03-29 17:17:11 -0400

    Handle vlans on Linux

--- modules/wr/lib/facter/ipaddress_primary.rb
+++ modules/wr/lib/facter/ipaddress_primary.rb
@@ -1,21 +1,8 @@
-# from https://github.com/panaman/puppet-hostint/blob/master/lib/facter/hostint.rb
-ktype = (Facter.value('Kernel'))
 Facter.add("ipaddress_primary") do
-  confine :kernel => %w{Linux Darwin FreeBSD}
-  if ktype == 'FreeBSD'
-    setcode do
-      int = Facter::Util::Resolution.exec("netstat -f inet -rn | awk '$1==\"default\" { print $6 }'")
-      Facter.value("ipaddress_#{int}")
-    end
-  elsif ktype == 'Darwin'
-    setcode do
-      int = Facter::Util::Resolution.exec("netstat -f inet -rn | awk '$1==\"default\" { print $6 }'")
-      Facter.value("ipaddress_#{int}")
-    end
-  elsif ktype == 'Linux'
-    setcode do
-      int = Facter::Util::Resolution.exec("ip route | grep default | awk '{print $5}'")
-      Facter.value("ipaddress_#{int}")
-    end
+  confine :kernel => %w{Linux}
+  setcode do
+    # tr is needed to handle vlan addresses
+    int = Facter::Util::Resolution.exec("ip route | grep default | awk '{print $5}'").tr('.', '_')
+    Facter.value("ipaddress_#{int}")
   end
 end
