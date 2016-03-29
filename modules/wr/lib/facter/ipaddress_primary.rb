19d6d0a6c46f6b56ce2e100d390f7f546f45c37b
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-03-29 16:53:38 -0400

    Hack to workaround ip detection with vlan

--- modules/wr/lib/facter/ipaddress_primary.rb
+++ modules/wr/lib/facter/ipaddress_primary.rb
@@ -4,6 +4,8 @@ Facter.add("ipaddress_primary") do
       Facter.value('ipaddress_eth0')
     elsif Facter.value('ipaddress_em1')
       Facter.value('ipaddress_em1')
+    elsif Facter.value('ipaddress_em1_105')
+      Facter.value('ipaddress_em1_105')
     else
       Facter.value('ipaddress')
     end
