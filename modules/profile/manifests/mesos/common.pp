c320a28b84d46b69228032156facab97e37d058c
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-04-01 15:10:41 -0400

    Install dnsmasq on mesos hosts
    This will be used to expose consul DNS and local /etc/hosts entries to
    containers

--- modules/profile/manifests/mesos/common.pp
+++ modules/profile/manifests/mesos/common.pp
@@ -82,4 +82,22 @@ class profile::mesos::common inherits profile::nis {
       mode   => '0755',
       source => 'puppet:///modules/wr/docker-rc.local';
   }
+
+  # Use dnsmasq to expose consul dns to host and container DNS lookups
+  include ::dnsmasq
+
+  # Any lookups to .consul domain are redirected to consul DNS port
+  ::dnsmasq::conf {
+    'consul':
+      ensure  => present,
+      content => 'server=/consul/127.0.0.1#8600',
+  }
+
+  # Expose dnsmasq on port 53 only to localhost and on docker bridge
+  # This requires that docker daemon is configured with dns as bridge interface ip
+  ::dnsmasq::conf {
+    'docker':
+      ensure  => present,
+      content => "interface=lo\ninterface=docker0\nbind-interfaces",
+  }
 }
