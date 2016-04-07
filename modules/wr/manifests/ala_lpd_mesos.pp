8f5484253eed4db001c4de40af75027081c394b1
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-04-07 15:46:45 -0400

    Run the wraxl scheduler in a docker container on ala-lpd-mesos

--- modules/wr/manifests/ala_lpd_mesos.pp
+++ modules/wr/manifests/ala_lpd_mesos.pp
@@ -6,4 +6,17 @@ class wr::ala_lpd_mesos {
 
   #running zookeeper in docker means zookeeper will restart when docker restarts
   include zookeeper
+
+  include ::profile::docker
+
+  docker::run {
+    'wraxl_scheduler':
+      image           => 'wr-docker-registry:5000/mesos-scheduler:0.25.0',
+      command         => "--master 'zk://147.11.106.56:2181,147.11.105.21:2181,147.11.105.120:2181/mesos'",
+      ports           => ['8080:8080'],
+      hostname        => $::fqdn,
+      restart_service => true,
+      pull_on_start   => true,
+      require         => [Docker::Image['mesos-scheduler'],Service['docker']];
+  }
 }
