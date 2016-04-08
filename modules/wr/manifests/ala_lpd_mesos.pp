43900af95cc22e5b8c201aacfd8955cc10e3e6e3
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-04-08 10:42:17 -0400

    Pass the ip address of the scheduler as param
    Using a name resolves to an internal ip inside the docker container and
    gets sent to the master

--- modules/wr/manifests/ala_lpd_mesos.pp
+++ modules/wr/manifests/ala_lpd_mesos.pp
@@ -12,12 +12,11 @@ class wr::ala_lpd_mesos {
   docker::run {
     'wraxl_scheduler':
       image           => 'wr-docker-registry:5000/mesos-scheduler:0.25.0',
-      command         => "--master 'zk://147.11.106.56:2181,147.11.105.21:2181,147.11.105.120:2181/mesos'",
+      command         => "--master zk://147.11.106.56:2181,147.11.105.21:2181,147.11.105.120:2181/mesos --hostname ${::ipaddress_primary}",
       ports           => ['8080:8080'],
       hostname        => $::fqdn,
       restart_service => true,
       pull_on_start   => true,
-      env             => ["LIBPROCESS_IP=${::ipaddress_primary}"],
       volumes         => ['/home/wrlbuild/wr-buildscripts:/mnt/'],
       require         => [Service['docker']];
   }
