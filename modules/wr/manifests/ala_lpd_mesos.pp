a0c8c72ecbe1da39d00e329fce58624f44da6a03
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-04-29 15:59:08 -0400

    Add log rotation to scheduler output

--- modules/wr/manifests/ala_lpd_mesos.pp
+++ modules/wr/manifests/ala_lpd_mesos.pp
@@ -11,14 +11,14 @@ class wr::ala_lpd_mesos {
 
   docker::run {
     'wraxl_scheduler':
-      image           => 'wr-docker-registry:5000/mesos-scheduler:0.25.0',
-      command         => '--master zk://147.11.106.56:2181,147.11.105.21:2181,147.11.105.120:2181/mesos --config /mnt/random_coverage_config.yaml &> /var/log/upstart/wraxl_scheduler.log',
-      ports           => ['8080:8080'],
-      net             => 'host',
-      restart_service => true,
-      pull_on_start   => true,
-      volumes         => ['/home/wrlbuild/wr-buildscripts:/mnt/',
-                          '/var/log:/var/log/'],
-      require         => [Service['docker']];
+      image            => 'wr-docker-registry:5000/mesos-scheduler:0.25.0',
+      command          => '--master zk://147.11.106.56:2181,147.11.105.21:2181,147.11.105.120:2181/mesos --config /mnt/random_coverage_config.yaml',
+      ports            => ['8080:8080'],
+      net              => 'host',
+      restart_service  => true,
+      pull_on_start    => true,
+      volumes          => ['/home/wrlbuild/wr-buildscripts:/mnt/'],
+      extra_parameters => ['--log-opt max-size=1m', '--log-opt max-file=7' ],
+      require          => [Service['docker']];
   }
 }
