a3deadf75d6b620cb0bc7dc8582650e56d26f8d2
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-07-14 11:29:14 -0400

    Expose dnsmasq on all interfaces
    During puppet runs changes to dnsmasq caused lookup failures. Also if
    docker was not installed dnsmasq server startup would also fail. Also
    when docker compose/networking is used, the network is on a different
    bridge which meant that networking would timeout and fail inside the
    containers. This does expose dnsmasq to all traffic and is a potential
    security issue.

--- modules/profile/manifests/docker/dnsmasq.pp
+++ modules/profile/manifests/docker/dnsmasq.pp
@@ -9,12 +9,4 @@ class profile::docker::dnsmasq {
       ensure  => present,
       content => 'server=/consul/127.0.0.1#8600',
   }
-
-  # Expose dnsmasq on port 53 only to localhost and on docker bridge
-  # This requires that docker daemon is configured with dns as bridge interface ip
-  ::dnsmasq::conf {
-    'docker':
-      ensure  => present,
-      content => "interface=lo\ninterface=docker0\nbind-interfaces",
-  }
 }
