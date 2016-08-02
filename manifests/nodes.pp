479fe7dc5cce4598eb320f6398860d1118245dd3
Konrad Scherer <Konrad.Scherer@windriver.com>
2016-08-02 14:21:16 -0400

    Move ala-blade 42-45 to wraxl

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -27,7 +27,7 @@ node 'ala-blade17.wrs.com' {
   include profile::lava
 }
 
-node /ala-blade([1-9]|19|31|3[3-9]|4[0-1]|49|5[0-9]|6[0-9])\.wrs\.com/ {
+node /ala-blade([1-9]|19|31|3[3-9]|4[0-5]|49|5[0-9]|6[0-9])\.wrs\.com/ {
   include profile::mesos::slave
 }
 
